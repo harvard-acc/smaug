208d6763bff5c4f48c38c0e7379ee152051701db
Sam Xi <samxi@seas.harvard.edu>
2017-02-16 11:29:37 -0500

    Working 3 layer DNN (FC layers only).
    This should be representative of the final Minerva network.

---
+++ mnist/sim/DNN-3FC/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cfg_home=`pwd`
+gem5_dir=${ALADDIN_HOME}/../..
+bmk_dir=/home/samxi/active_projects/composable/mnist/build
+
+${gem5_dir}/build/X86/gem5.opt \
+  --debug-flags=Aladdin,HybridDatapath \
+  --outdir=${cfg_home}/outputs \
+  ${gem5_dir}/configs/aladdin/aladdin_se.py \
+  --num-cpus=1 \
+  --mem-size=4GB \
+  --mem-type=DDR3_1600_x64  \
+  --sys-clock=1GHz \
+  --cpu-type=detailed \
+  --caches \
+  --cacheline_size=32 \
+  --accel_cfg_file=${cfg_home}/gem5.cfg \
+  -c nnet-gem5-accel \
+  | gzip -c > stdout.gz
