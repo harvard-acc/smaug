b5a41b060745a28598df8eb85539aa62e0e77fbd
Sam Xi <samxi@seas.harvard.edu>
2017-02-17 11:43:44 -0500

    Update all sim directories to new config format.
    Binary executabes are no longer needed since the models are encoded in
    the conf files now.

--- nnet_lib/sim/CNN-1C-1P-1FC/run.sh
+++ nnet_lib/sim/CNN-1C-1P-1FC/run.sh
@@ -2,6 +2,7 @@
 
 cfg_home=`pwd`
 gem5_dir=${ALADDIN_HOME}/../..
+bmk_dir=/home/samxi/active_projects/composable/nnet_lib/build
 
 ${gem5_dir}/build/X86/gem5.opt \
   --debug-flags=Aladdin,HybridDatapath \
@@ -15,5 +16,6 @@ ${gem5_dir}/build/X86/gem5.opt \
   --caches \
   --cacheline_size=32 \
   --accel_cfg_file=${cfg_home}/gem5.cfg \
-  -c nnet-gem5-accel \
+  -c ${bmk_dir}/nnet-gem5-accel \
+  -o ../../../models/generic/cnn-1c-1p-1fc.conf \
   | gzip -c > stdout.gz
--- nnet_lib/sim/DNN-3FC/run.sh
+++ nnet_lib/sim/DNN-3FC/run.sh
@@ -2,7 +2,7 @@
 
 cfg_home=`pwd`
 gem5_dir=${ALADDIN_HOME}/../..
-bmk_dir=/home/samxi/active_projects/composable/mnist/build
+bmk_dir=/home/samxi/active_projects/composable/nnet_lib/build
 
 ${gem5_dir}/build/X86/gem5.opt \
   --debug-flags=Aladdin,HybridDatapath \
@@ -16,5 +16,6 @@ ${gem5_dir}/build/X86/gem5.opt \
   --caches \
   --cacheline_size=32 \
   --accel_cfg_file=${cfg_home}/gem5.cfg \
-  -c nnet-gem5-accel \
+  -c ${bmk_dir}/nnet-gem5-accel \
+  -o ../../../models/mnist/minerva.conf \
   | gzip -c > stdout.gz
