c679b7c56ef5d6408d0e4b296d06017365c94fca
Sam Xi <samxi@seas.harvard.edu>
2017-02-22 11:10:41 -0500

    Move the existing sim directory out of nnet_lib.
    nnet_lib should be just source code. sim now lives in its own top level
    directory, and all existing directories underneath that have been moved
    to sim/monolithic (since all the traces are monolithic).
    
    Also, LeNet5-ish sim was added.

---
+++ sim/monolithic/LeNet5-ish/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cfg_home=`pwd`
+gem5_dir=${ALADDIN_HOME}/../..
+bmk_dir=/home/samxi/active_projects/composable/nnet_lib/build
+
+${gem5_dir}/build/X86/gem5.opt \
+  --debug-flags=Aladdin,HybridDatapath \
+  --outdir=${cfg_home}/outputs \
+  ${gem5_dir}/configs/aladdin/aladdin_se.py \
+  --num-cpus=1 \
+  --mem-size=4GB \
+  --mem-type=DDR3_1600_x64  \
+  --sys-clock=1GHz \
+  --cpu-type=detailed \
+  --record-dram-traffic \
+  --caches \
+  --cacheline_size=32 \
+  --accel_cfg_file=${cfg_home}/gem5.cfg \
+  -c ${bmk_dir}/nnet-gem5-accel \
+  -o ../../../models/mnist/lenet5-ish.conf \
+  | gzip -c > stdout.gz
