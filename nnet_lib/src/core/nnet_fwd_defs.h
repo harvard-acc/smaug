0b6a7242221ec926a59230d361a79461659261df
Sam Xi <samxi@seas.harvard.edu>
2017-06-04 10:42:39 -0400

    Clarify the usage of ARRAY_XD macros.

--- nnet_lib/src/core/nnet_fwd_defs.h
+++ nnet_lib/src/core/nnet_fwd_defs.h
@@ -258,6 +258,18 @@ typedef struct _iarray_t {
 // Use these convenience macros to cast a raw pointer into a multidimensional
 // variable-length array, which lets us use [] notation inside of the ugly
 // sub2ind syntax!
+//
+// Usage:
+//   If we have an array like array[5][4]:
+//      ARRAY_2D(TYPE, output_name, array, 4);
+//
+//   If we have an array like array[5][4][3]:
+//      ARRAY_3D(TYPE, output_name, array, 4, 3);
+//
+//   If we have an array like array[5][4][3][2]
+//      ARRAY_4D(TYPE, output_name, array, 4, 3, 2);
+//
+//   And so on...
 #define ARRAY_2D(TYPE, output_array_name, input_array_name, DIM_1)             \
     TYPE(*output_array_name)[DIM_1] = (TYPE(*)[DIM_1])input_array_name
 
