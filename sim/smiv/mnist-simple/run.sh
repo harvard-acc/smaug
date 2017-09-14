1c4bc71638a5a19282154675673889bdb599dfd8
Sam Xi <samxi@seas.harvard.edu>
2017-09-14 18:03:13 -0400

    smiv: Add a slightly larger test with MNIST-sized input.
    Includes a pooling layer in between the CONV and FC layers.

---
+++ sim/smiv/mnist-simple/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cfg_home=`pwd`
+gem5_dir=${ALADDIN_HOME}/../..
+bmk_dir=/home/samxi/active_projects/composable/nnet_lib/build
+
+${gem5_dir}/build/X86/gem5.opt \
+  --debug-flags=Aladdin,HybridDatapath \
+  --outdir=${cfg_home}/outputs \
+  ${gem5_dir}/configs/aladdin/aladdin_se.py \
+  --num-cpus=1 \
+  --mem-size=4GB \
+  --mem-type=DDR3_1600_8x8  \
+  --sys-clock=1GHz \
+  --cpu-type=DerivO3CPU \
+  --caches \
+  --cacheline_size=32 \
+  --accel_cfg_file=${cfg_home}/gem5.cfg \
+  -c ${bmk_dir}/nnet-gem5-accel \
+  -o mnist-simple.conf \
+  | gzip -c > stdout.gz
