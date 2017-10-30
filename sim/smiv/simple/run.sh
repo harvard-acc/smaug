7edac9a9058330f5f77b75e9aa5752d05d978e3a
Sam Xi <samxi@seas.harvard.edu>
2017-10-30 15:54:12 -0400

    smiv: Updated simple test after DMA v3 and other changes.
    Working functionally and performance is roughly where I would expect.

--- sim/smiv/simple/run.sh
+++ sim/smiv/simple/run.sh
@@ -9,12 +9,12 @@ ${gem5_dir}/build/X86/gem5.opt \
   --outdir=${cfg_home}/outputs \
   ${gem5_dir}/configs/aladdin/aladdin_se.py \
   --num-cpus=1 \
-  --mem-size=4GB \
+  --mem-size=8GB \
   --mem-type=DDR3_1600_8x8  \
   --sys-clock=1GHz \
   --cpu-type=DerivO3CPU \
   --caches \
-  --cacheline_size=32 \
+  --cacheline_size=64 \
   --accel_cfg_file=${cfg_home}/gem5.cfg \
   -c ${bmk_dir}/nnet-gem5-accel \
   -o simple.conf \
