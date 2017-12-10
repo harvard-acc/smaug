bf92d0c14e836bdb354fd19ac36d71c963a8751b
Sam Xi <samxi@seas.harvard.edu>
2017-12-10 17:27:23 -0500

    eigen: Improved pooling implementation.
    Although the performance is better than before, unfortunately, here is
    where I discovered that Eigen's Tensor module does not support
    vectorization, which explains why our performance is so terrible
    compared to the naive versions.
    
    Change-Id: Ic885eda9c8068f87e32de11ace880575c49b958b

--- nnet_lib/src/core/eigen/pooling.cpp
+++ nnet_lib/src/core/eigen/pooling.cpp
@@ -7,7 +7,7 @@
 #include "core/nnet_fwd_defs.h"
 #include "core/pooling.h"
 #include "core/eigen/pooling.h"
-#include "utility/utility.h"
+#include "utility/eigen_utility.h"
 
 namespace nnet_eigen {
 
@@ -119,28 +119,23 @@ void max_pooling_colmajor(float* activations, float* result, layer_t curr_layer)
     int stride = curr_layer.field_stride;
     int size = curr_layer.weights.cols;
 
-    // Input activations are always in NCHW format, RowMajor.
-    typedef Tensor<float, 4, RowMajor> InputTensorType;
-    typedef Tensor<float, 4, ColMajor> ColInputTensorType;
-    TensorMap<InputTensorType> inputs_map(
+    // Input activations are always in NCHW format, ColMajor.
+    typedef Tensor<float, 4, ColMajor> InputTensorType;
+    TensorMap<InputTensorType, Aligned64> inputs_map(
             activations, NUM_TEST_CASES, hgt, in_rows, in_cols + in_pad);
-    TensorMap<ColInputTensorType> results_map(
-            result, out_cols + out_pad, out_rows, hgt, NUM_TEST_CASES);
+    TensorMap<InputTensorType, Aligned64> results_map(
+            result, NUM_TEST_CASES, hgt, out_rows, out_cols + out_pad);
 
-#if DEBUG_LEVEL >= 2
-    for (int n = 0; n < NUM_TEST_CASES; n++) {
-        std::cout << "Input image " << n << "\n";
-        for (int h = 0; h < hgt; h++) {
-            std::cout << "Channel " << h << "\n";
-            for (int r = 0; r < in_rows; r++) {
-                for (int c = 0; c < in_cols + in_pad; c++) {
-                    printf("%4.6f ", inputs_map(n, h, r, c));
-                }
-                std::cout << "\n";
-            }
-        }
-    }
+#if DEBUG_LEVEL >= 1
+    print_debug4d(inputs_map);
 #endif
+
+    // Mold the input volume patches into a shape suitable for reduction. Post
+    // reduction, we should end up with a 2D matrix.
+    Tensor<float, 3>::Dimensions prereduce_dims;
+    prereduce_dims[0] = NUM_TEST_CASES;
+    prereduce_dims[1] = size * size;
+    prereduce_dims[2] = hgt * out_rows * out_cols;
 
     // Postreduce dims = final output shape.
     Tensor<float, 4>::Dimensions postreduce_dims;
@@ -153,56 +148,51 @@ void max_pooling_colmajor(float* activations, float* result, layer_t curr_layer)
     postreduce_dims[idxRow] = out_rows;
     postreduce_dims[idxCol] = out_cols + out_pad;
 
-    // Shape suitable for reduction. Post reduction, we should end up with a 2D matrix.
-    Tensor<float, 3>::Dimensions prereduce_dims;
-    prereduce_dims[0] = postreduce_dims[0];
-    prereduce_dims[1] = size * size;
-    prereduce_dims[2] = postreduce_dims[1] * postreduce_dims[2] * postreduce_dims[3];
-
     // The axis along which we will reduce (equal to the one that is size *
     // size).
     Tensor<float, 1>::Dimensions reduction_dims(1);
 
-    // The data arrives in row major order. We need to turn it into column
-    // major, but we need to preserve the data layout of NCHW. swap_layout()
-    // will convert rowmajor to colmajor, but it will also reverse the
-    // dimensionality, so the shuffles will reverse it right back.
-    Eigen::array<ptrdiff_t, 4> preshuffle_idx({3,2,1,0});
+#if DEBUG_LEVEL >= 1
+    // Print out every step of the reduction.
+    Tensor<float, 5> volume_patches = inputs_map.extract_volume_patches(
+            1, size, size, 1, stride, stride, PADDING_VALID);
+    for (int i : volume_patches.dimensions())
+      std::cout << i << ",";
+    std::cout << "\nVolume patches:\n" << volume_patches << "\n";
 
-    // The patches get extracted interleaved across channels (unsure about
-    // input batching right now), so this shuffle operation undoes the effect
-    // of that.
-    Eigen::array<ptrdiff_t, 4> postshuffle_idx({3,2,1,0});
+    Tensor<float, 3> prereduced = volume_patches.reshape(prereduce_dims);
+    for (int i : prereduced.dimensions())
+      std::cout << i << ",";
+    std::cout << "\nReshaped:\n" << prereduced << "\n";
 
-    results_map = inputs_map.shuffle(preshuffle_idx)
-                          .swap_layout()
+    Tensor<float, 2> reduced = prereduced.maximum(reduction_dims);
+    for (int i : reduced.dimensions())
+      std::cout << i << ",";
+    std::cout << "\nReduced:\n" << reduced << "\n";
+
+    Tensor<float, 4> postreduced = reduced.reshape(postreduce_dims);
+    for (int i : postreduced.dimensions())
+      std::cout << i << ",";
+    std::cout << "\nReshaped:\n" << postreduced << "\n";
+
+    results_map = postreduced;
+#else
+    results_map = inputs_map
                           .extract_volume_patches(1, size, size, 1, stride,
                                                   stride, PADDING_VALID)
                           .reshape(prereduce_dims)
                           .maximum(reduction_dims)
-                          .reshape(postreduce_dims)
-                          .shuffle(postshuffle_idx);
+                          .reshape(postreduce_dims);
+
+#endif
+
 #if DEBUG_LEVEL >= 1
-    for (int n = 0; n < NUM_TEST_CASES; n++) {
-        std::cout << "Final output.\n";
-        for (int h = 0; h < hgt; h++) {
-            std::cout << "channel " << h << "\n";
-            for (int r = 0; r < out_rows; r++) {
-                for (int c = 0; c < out_cols + out_pad; c++) {
-                    // Since the final data MUST be in row major order, but
-                    // this map is colmajor, we have to manually reverse the
-                    // indices.
-                    printf("%4.6f ", results_map(c, r, h, n));
-                }
-                std::cout << "\n";
-            }
-        }
-    }
+    print_debug4d(results_map);
 #endif
 }
 
 void max_pooling(float* activations, float* result, layer_t curr_layer) {
-    max_pooling_rowmajor(activations, result, curr_layer);
+    max_pooling_colmajor(activations, result, curr_layer);
 }
 
 }  // namespace nnet_eigen
--- nnet_lib/src/perftests/test_eigen_maxpooling.cpp
+++ nnet_lib/src/perftests/test_eigen_maxpooling.cpp
@@ -1,30 +1,31 @@
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+#include "unsupported/Eigen/CXX11/Tensor"
 
 #include "core/pooling.h"
 #include "core/eigen/pooling.h"
+#include "utility/init_eigen_data.h"
 #include "utility/init_data.h"
+#include "utility/eigen_utility.h"
 #include "utility/utility.h"
 #include "utility/profiling.h"
 
 #include "nnet_fwd.h"
 
 int INPUT_DIM;
-int NUM_TEST_CASES = 1;
+int NUM_TEST_CASES = 2;
 int NUM_CLASSES;
 float* sigmoid_table;
 
-enum MODE {
-  ROWMAJOR,
-  COLMAJOR,
-  MANUAL
-};
+enum MODE { eigen, manual };
 
 void print_help() {
     printf("Usage: ./test_eigen_maxpooling mode\n"
-           "  mode: rowmajor, colmajor, or manual\n");
+           "  mode: eigen or manual\n");
 }
 
 int main(int argc, const char* argv[]) {
@@ -35,49 +36,77 @@ int main(int argc, const char* argv[]) {
 
     const char* mode_str = argv[1];
     MODE mode;
-    if (strncmp(mode_str, "rowmajor", 8) == 0) {
-        mode = ROWMAJOR;
-    } else if (strncmp(mode_str, "colmajor", 8) == 0) {
-        mode = COLMAJOR;
+    if (strncmp(mode_str, "eigen", 5) == 0) {
+        mode = eigen;
     } else if (strncmp(mode_str, "manual", 6) == 0) {
-        mode = MANUAL;
+        mode = manual;
     } else {
         print_help();
         return 1;
     }
 
-    int iterations = 2; //256*8;
+    int iterations = 100;
     int in_rows = 224, in_cols = 224, chans = 3;
     int pool_size = 2;
     int pool_stride = 2;
     int out_rows = (in_rows - pool_size) / pool_stride + 1;
     int out_cols = (in_cols - pool_size) / pool_stride + 1;
-    size_t total_in_size = in_rows * in_cols * chans;
-    size_t total_out_size = out_rows * out_cols * chans;
+    size_t total_in_size = in_rows * in_cols * chans * NUM_TEST_CASES;
+    size_t total_out_size = out_rows * out_cols * chans * NUM_TEST_CASES;
     float* inputs = (float*)malloc_aligned(total_in_size * sizeof(float));
     float* results = (float*)malloc_aligned(total_out_size * sizeof(float));
 
-    layer_t fake_layer;
-    fake_layer.type = POOLING;
-    fake_layer.pool = MAX;
-    fake_layer.inputs = {in_rows, in_cols, chans, 0};
-    fake_layer.outputs = {out_rows, out_cols, chans, 0};
-    fake_layer.weights = {pool_size, pool_size, 1, 0};
-    fake_layer.field_stride = pool_stride;
+    network_t network;
+    layer_t layer;
+    layer.type = POOLING;
+    layer.pool = MAX;
+    layer.inputs = {in_rows, in_cols, chans, 0};
+    layer.outputs = {out_rows, out_cols, chans, 0};
+    layer.weights = {pool_size, pool_size, 1, 0};
+    layer.field_stride = pool_stride;
+    network.layers = &layer;
+    network.depth = 1;
 
-    if (mode == ROWMAJOR) {
+    if (mode == eigen) {
+        nnet_eigen::init_data(inputs, &network, NUM_TEST_CASES, FIXED);
+    } else {
+        init_data(inputs, &network, NUM_TEST_CASES, FIXED);
+    }
+
+    std::cout << "\n==========================\n";
+    auto start = std::chrono::high_resolution_clock::now();
+
+    if (mode == eigen) {
         for (int it = 0; it < iterations; it++) {
-            nnet_eigen::max_pooling_rowmajor(inputs, results, fake_layer);
+            nnet_eigen::max_pooling(inputs, results, layer);
         }
 
-    } else if (mode == COLMAJOR) {
-        for (int it = 0; it < iterations; it++) {
-            nnet_eigen::max_pooling_colmajor(inputs, results, fake_layer);
-        }
     } else {
         for (int it = 0; it < iterations; it++) {
-            max_pooling_image3d(inputs, 0, results, fake_layer);
+            max_pooling(inputs, results, layer);
         }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << "s\n\n";
+
+    // Print something about the result.
+    if (layer.outputs.rows <= 10) {
+        // If it's not too large, then print the actual result.
+        if (mode == eigen) {
+            Eigen::TensorMap<Eigen::Tensor<float, 4>> map(
+                    results, NUM_TEST_CASES, chans, out_rows, out_cols);
+            nnet_eigen::print_debug4d(map);
+        } else {
+            print_debug4d(results, out_rows, out_cols, chans);
+        }
+    } else {
+        // Otherwise, just print the sum of the result.
+        float sum = 0;
+        for (unsigned i = 0; i < total_out_size; i++)
+            sum += results[i];
+        printf("sum = %f\n", sum);
     }
 
     free(inputs);
