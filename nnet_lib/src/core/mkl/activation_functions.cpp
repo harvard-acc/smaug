b5d9105fe8764ac868d9264b3031827cfb43aede
Sam Xi <samxi@seas.harvard.edu>
2017-12-17 18:38:25 -0500

    mkl: Major refactor of the activation functions.
    All primitives are now managed by a parent ActivationFunctionOp class,
    and specific activation functions are implemented simply by inheriting
    the parent class and defining a new constructor. A way to directly
    access the output memory primitive is made available to reduce primitive
    construction overhead.
    
    Change-Id: Ib7dd0e724e91b7d382f0370571caa8684d984701

--- nnet_lib/src/core/mkl/activation_functions.cpp
+++ nnet_lib/src/core/mkl/activation_functions.cpp
@@ -1,3 +1,6 @@
+#include <memory>
+#include <vector>
+
 #include "activation_functions.h"
 
 namespace nnet_mkl {
@@ -5,43 +8,13 @@ namespace nnet_mkl {
 using namespace mkldnn;
 
 void sigmoid(float* activations, int size, engine& cpu, float* results) {
-    std::vector<primitive> network;
-
-    memory::dims dims = { size };
-    auto input_md = mem_d(
-            { dims }, memory::data_type::f32, memory::format::x);
-    auto input_mempd = mem_pd(input_md, cpu);
-    auto input_memory = memory(input_mempd, activations);
-    auto sigmoid_desc = eltwise_forward::desc(
-            prop_kind::forward, algorithm::eltwise_logistic, input_md, 0, 0);
-    auto sigmoid_pd = eltwise_forward::primitive_desc(sigmoid_desc, cpu);
-    auto sigmoid_dst_memory = memory(sigmoid_pd.dst_primitive_desc(), results);
-    network.emplace_back(
-            eltwise_forward(sigmoid_pd, input_memory, sigmoid_dst_memory));
-    stream(stream::kind::eager).submit(network).wait();
+    SigmoidActivationFunctionOp sigmoid_op(activations, results, size, cpu);
+    sigmoid_op.run();
 }
 
 void relu(float* activations, int size, engine& cpu, float* results) {
-    static const double negative_slope = 0;
-    std::vector<primitive> network;
-
-    memory::dims dims = { size };
-    auto input_md = mem_d(
-            { dims }, memory::data_type::f32, memory::format::x);
-    auto input_mempd  = mem_pd(input_md, cpu);
-    auto input_memory = memory(input_mempd, activations);
-    auto output_md = mem_d(
-            { dims }, memory::data_type::f32, memory::format::x);
-    auto output_mempd  = mem_pd(output_md, cpu);
-    auto output_memory = memory(output_mempd, results);
-    auto relu_desc = eltwise_forward::desc(prop_kind::forward,
-                                           algorithm::eltwise_relu,
-                                           input_md,
-                                           negative_slope);
-    auto relu_pd = eltwise_forward::primitive_desc(relu_desc, cpu);
-    network.emplace_back(
-            eltwise_forward(relu_pd, input_memory, output_memory));
-    stream(stream::kind::eager).submit(network).wait();
+    ReluActivationFunctionOp relu_op(activations, results, size, cpu);
+    relu_op.run();
 }
 
 void activation_fun(float* activations,
--- nnet_lib/src/core/mkl/activation_functions.h
+++ nnet_lib/src/core/mkl/activation_functions.h
@@ -7,6 +7,125 @@
 
 namespace nnet_mkl {
 
+template <typename DType>
+class ActivationFunctionOp {
+   public:
+    using mem_index_t = int;
+
+    ActivationFunctionOp(mkldnn::engine& eng) : engine(eng), output_idx(-1) {}
+    virtual ~ActivationFunctionOp() {}
+
+    // Execute the activation function.
+    void run() {
+        mkldnn::stream(mkldnn::stream::kind::eager).submit(worklist).wait();
+    }
+
+    // Return the list of primitives.
+    //
+    // This is useful if execution should be delayed.
+    std::vector<mkldnn::primitive>& get_worklist() { return worklist; }
+
+    // Return the memory primitive containing the output of this operation.
+    mkldnn::memory& get_output_memory() { return memories.at(output_idx); }
+
+   protected:
+    // Create and return an index to a memory primitive.
+    //
+    // If @buffer is not NULL, then the memory primitive will use that pointer;
+    // otherwise, it will allocate its own memory.
+    //
+    // The memory primitive is stored in a list inside this object. The index
+    // corresponds to the location of the primitive in this list. It is
+    // guaranteed to be stable during the lifetime of this Op object.
+    virtual mem_index_t create_memory(DType* buffer,
+                                      int size,
+                                      bool is_output = false) {
+        mkldnn::memory::dims dims = { size };
+        auto md = mem_d({ dims },
+                        mkl_traits<DType>::mfmt,
+                        mkldnn::memory::format::x);
+        auto mempd = mem_pd(md, engine);
+        if (buffer)
+            memories.emplace_back(mempd, buffer);
+        else
+            memories.emplace_back(mempd);
+        mem_index_t retval = memories.size() - 1;
+        if (is_output)
+            output_idx = retval;
+        return retval;
+    }
+
+    // Create a return a reference to the operation primitive.
+    virtual mkldnn::eltwise_forward& create_primitive(mkldnn::algorithm alg,
+                                                      mem_index_t input_idx,
+                                                      mem_index_t output_idx,
+                                                      DType alpha = 0,
+                                                      DType beta = 0) {
+        auto& input = memories.at(input_idx);
+        auto& output = memories.at(output_idx);
+        auto desc =
+                mkldnn::eltwise_forward::desc(mkldnn::prop_kind::forward,
+                                              alg,
+                                              input.get_primitive_desc().desc(),
+                                              alpha,
+                                              beta);
+        auto pd = mkldnn::eltwise_forward::primitive_desc(desc, engine);
+        worklist.emplace_back(mkldnn::eltwise_forward(pd, input, output));
+        return static_cast<mkldnn::eltwise_forward&>(worklist.back());
+    }
+
+   protected:
+    // The execution engine.
+    const mkldnn::engine& engine;
+
+    // A list of all the memory objects required. These cannot be destroyed
+    // before the list of primitives has been executed!
+    std::vector<mkldnn::memory> memories;
+
+    // A list of primitives to execute.
+    std::vector<mkldnn::primitive> worklist;
+
+    // The index of the memory storing the output.
+    //
+    // Subsequent operations can use this to directly get a reference to the
+    // memory primitive, instead of creating a new one.
+    mem_index_t output_idx;
+};
+
+class ReluActivationFunctionOp : public ActivationFunctionOp<dtype> {
+   public:
+    ReluActivationFunctionOp(dtype* input_buffer,
+                             dtype* output_buffer,
+                             int size,
+                             mkldnn::engine& engine,
+                             dtype negative_slope = 0)
+            : ActivationFunctionOp(engine) {
+        auto input_mem_index = create_memory(input_buffer, size);
+        auto output_mem_index = create_memory(output_buffer, size, true);
+        create_primitive(mkldnn::algorithm::eltwise_relu,
+                         input_mem_index,
+                         output_mem_index,
+                         negative_slope);
+    }
+    virtual ~ReluActivationFunctionOp() {}
+};
+
+class SigmoidActivationFunctionOp : public ActivationFunctionOp<dtype> {
+   public:
+    SigmoidActivationFunctionOp(dtype* input_buffer,
+                                dtype* output_buffer,
+                                int size,
+                                mkldnn::engine& engine)
+            : ActivationFunctionOp(engine) {
+        auto input_mem_index = create_memory(input_buffer, size);
+        auto output_mem_index = create_memory(output_buffer, size, true);
+        create_primitive(mkldnn::algorithm::eltwise_logistic,
+                         input_mem_index,
+                         output_mem_index);
+    }
+    virtual ~SigmoidActivationFunctionOp() {}
+};
+
 void sigmoid(float* activations, int size, mkldnn::engine& cpu, float* results);
 void relu(float* activations, int size, mkldnn::engine& cpu, float* results);
 void activation_fun(float* activations,
@@ -14,6 +133,6 @@ void activation_fun(float* activations,
                     activation_type function,
                     float* results,
                     device_t* device);
-}
+}  // namespace nnet_mkl
 
 #endif
