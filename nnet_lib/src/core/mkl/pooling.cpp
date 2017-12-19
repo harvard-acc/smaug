6bf9c8cf3d5c2e1559484770f02d2244c47898ac
Sam Xi <samxi@seas.harvard.edu>
2017-12-19 15:06:15 -0500

    mkl: Implement MaxPoolingOp<DType>.
    Change-Id: I07dc41ba2359f084364509bc885e6408bddd63c7

--- nnet_lib/src/core/mkl/pooling.cpp
+++ nnet_lib/src/core/mkl/pooling.cpp
@@ -1,6 +1,6 @@
 #include "mkldnn.hpp"
 
-#include "pooling.h"
+#include "core/mkl/pooling.h"
 
 namespace nnet_mkl {
 
@@ -14,67 +14,9 @@ void max_pooling_3d(float* inputs,
     nnet_mkl::MklSession* session =
             reinterpret_cast<nnet_mkl::MklSession*>(device->session);
 
-    // Convert to MKL-DNN dimension descriptors.
-    memory::dims pool_input_dims = { NUM_TEST_CASES, curr_layer->inputs.height,
-                                     curr_layer->inputs.rows,
-                                     curr_layer->inputs.cols };
-    memory::dims pool_kernel = { curr_layer->weights.cols,
-                                 curr_layer->weights.cols };
-    memory::dims pool_strides = { curr_layer->field_stride,
-                                  curr_layer->field_stride };
-    memory::dims pool_padding = { 0, 0 };
-    memory::dims pool_output_dims = { NUM_TEST_CASES,
-                                      curr_layer->outputs.height,
-                                      curr_layer->outputs.rows,
-                                      curr_layer->outputs.cols };
-
-    // Create memory descriptors.
-    // Create primitive memory descriptors for user data.
-    // Create memory primitives for user input data.
-    auto user_input_md = mem_d(
-            { pool_input_dims }, memory::data_type::f32, memory::format::nchw);
-    auto user_input_memory_descriptor = mem_pd(user_input_md, session->cpu);
-    auto user_input_memory = memory(user_input_memory_descriptor, inputs);
-
-    // Create output memory descriptors.
-    auto user_output_md = mem_d(
-            { pool_output_dims }, memory::data_type::f32, memory::format::nchw);
-    auto user_output_memory_descriptor = mem_pd(user_output_md, session->cpu);
-    auto user_output_memory = memory(user_output_memory_descriptor, results);
-
-    // Create memory descriptors for the pooling primitive.
-    auto pool_output_md = mem_d(
-            { pool_output_dims }, memory::data_type::f32, memory::format::any);
-
-    // Create the pooling primitive descriptor.
-    auto pool_desc = pooling_forward::desc(
-            prop_kind::forward, algorithm::pooling_max, user_input_md,
-            pool_output_md, pool_strides, pool_kernel, pool_padding,
-            pool_padding, padding_kind::zero);
-    auto pool_pd = pooling_forward::primitive_desc(pool_desc, session->cpu);
-
-    // Pooling doesn't need input transformations...?
-
-    // Create memory primitives for the output.
-    auto pool_indices_memory = memory(pool_pd.workspace_primitive_desc());
-
-    // Make a separate output memory for pooling if required.
-    auto pool_output_memory = user_output_memory;
-    if (mem_pd(pool_pd.dst_primitive_desc()) != user_output_memory_descriptor) {
-        pool_output_memory = memory(pool_pd.dst_primitive_desc());
-    }
-
-    // Create the pooling primitive.
-    network.emplace_back(pooling_forward(pool_pd, user_input_memory,
-                                         pool_output_memory,
-                                         pool_indices_memory));
-
-    // Transform output if required.
-    if (pool_output_memory != user_output_memory) {
-        network.emplace_back(reorder(user_output_memory, pool_output_memory));
-    }
-
-    stream(stream::kind::eager).submit(network).wait();
+    MaxPoolingOp<dtype> pooling_op(
+            inputs, results, curr_layer, NUM_TEST_CASES, session->cpu);
+    pooling_op.run();
 }
 
 } // namespace nnet_mkl
--- nnet_lib/src/core/mkl/pooling.h
+++ nnet_lib/src/core/mkl/pooling.h
@@ -6,6 +6,108 @@
 
 namespace nnet_mkl {
 
+template <typename DType>
+class MaxPoolingOp : public BaseMklOp<DType> {
+   public:
+    MaxPoolingOp(DType* input_buffer,
+                 DType* output_buffer,
+                 layer_t* _layer,
+                 int _batch_size,
+                 mkldnn::engine& engine)
+            : BaseMklOp<DType>(engine), layer(_layer), batch_size(_batch_size) {
+        auto input_mem = create_input_memory(input_buffer);
+        auto output_mem = create_output_memory(output_buffer);
+
+        create_primitive(input_mem, output_mem);
+    }
+
+   protected:
+    // Return a mem_dims object for the input, assuming nchw format.
+    mem_dims get_input_dims() {
+        return { batch_size, layer->inputs.height, layer->inputs.rows,
+                 layer->inputs.cols };
+    }
+
+    // Return a mem_dims object for the output, assuming nchw format.
+    mem_dims get_output_dims() {
+        return { batch_size, layer->outputs.height, layer->outputs.rows,
+                 layer->outputs.cols };
+    }
+
+    // Return a mem_dims object for the pooling dims.
+    mem_dims get_pool_dims() {
+        return { layer->weights.cols, layer->weights.cols };
+    }
+
+    // Return a mem_dims object for the pooling strides.
+    mem_dims get_pool_strides() {
+        return { layer->field_stride, layer->field_stride };
+    }
+
+    // Return a mem_dims object for the pooling padding.
+    mem_dims get_pool_padding() { return { 0, 0 }; }
+
+    // Create an input memory primitive from a raw pointer.
+    //
+    // Returns the index to this primitive.
+    mem_ref_t create_input_memory(DType* buffer) {
+        return this->create_memory(buffer, get_input_dims(), mem_fmt::nchw);
+    }
+
+    // Create an output memory primitive from a raw pointer.
+    mem_ref_t create_output_memory(DType* buffer) {
+        return this->create_memory(
+                buffer, get_output_dims(), mem_fmt::nchw, true);
+    }
+
+    // Create an inner product primitive.
+    //
+    // Supply the memory indices for inputs, weights, bias, and outputs.
+    // Optionally, set force_output_format = true to force the output into nchw
+    // format; otherwise, it will simply use whatever format MKL-DNN decides.
+    mkldnn::primitive& create_primitive(mem_ref_t input, mem_ref_t output) {
+        auto pool_output_md = mem_d(
+                { get_output_dims() }, mkl_traits<DType>::dtype, mem_fmt::any);
+
+        // Create the pooling primitive descriptor.
+        auto pool_desc = mkldnn::pooling_forward::desc(
+                mkldnn::prop_kind::forward, mkldnn::algorithm::pooling_max,
+                input.get_primitive_desc().desc(), pool_output_md,
+                get_pool_strides(), get_pool_dims(), get_pool_padding(),
+                get_pool_padding(), mkldnn::padding_kind::zero);
+        auto pool_pd = mkldnn::pooling_forward::primitive_desc(
+                pool_desc, this->engine);
+
+        // Pooling requires a separate workspace memory.
+        // There seems to be a bug in MKL-DNN where we can't not use the
+        // workspace (or an exception gets thrown).
+        mem_ref_t workspace =
+                this->create_memory(pool_pd.workspace_primitive_desc());
+
+        // The output reorder needs to be delayed until after the operation
+        // primitive.
+        mkldnn::memory temp_output = output;
+        bool reordered = false;
+        if (this->needs_reorder(output, pool_pd.dst_primitive_desc())) {
+            temp_output = this->create_memory(pool_pd.dst_primitive_desc());
+            reordered = true;
+        }
+
+        this->worklist.emplace_back(mkldnn::pooling_forward(
+                pool_pd, input, temp_output, workspace));
+
+        if (reordered) {
+            this->worklist.emplace_back(mkldnn::reorder(temp_output, output));
+        }
+
+        return this->worklist.back();
+    }
+
+   // The pooling layer configuration.
+   const layer_t* layer;
+   const int batch_size;
+};
+
 void max_pooling_3d(float* inputs,
                     layer_t* curr_layer,
                     float* result,
