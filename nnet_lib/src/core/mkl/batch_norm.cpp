fbc23a59dac9bc759998d85ced7fc4ff00b37f8c
Sam Xi <samxi@seas.harvard.edu>
2017-12-19 14:18:27 -0500

    mkl: Implementation of BatchNormOp<DType>.
    Change-Id: If847ef4affc5eb2cf99ba9e04ec20df0cca41d02

--- nnet_lib/src/core/mkl/batch_norm.cpp
+++ nnet_lib/src/core/mkl/batch_norm.cpp
@@ -7,27 +7,6 @@ namespace nnet_mkl {
 
 using namespace mkldnn;
 
-// TODO: Move this into a common class shared by all the BN implementations.
-enum {
-    MEAN,
-    VARIANCE,
-    GAMMA,
-    BETA
-};
-
-// Get the appropriate dimensions for BN if the output is from an FC layer.
-memory::dims get_input_dims_for_fc_output(layer_t* curr_layer, int batch_size) {
-    int input_size = get_input_activations_size(curr_layer) / NUM_TEST_CASES;
-    return { batch_size, input_size, 1, 1 };
-}
-
-// Get the appropriate dimensions for BN if the output is from a CONV layer.
-memory::dims get_input_dims_for_conv_output(layer_t* curr_layer,
-                                            int batch_size) {
-    return { batch_size, curr_layer->inputs.height, curr_layer->inputs.rows,
-             curr_layer->inputs.cols };
-}
-
 void batch_norm(float* inputs,
                 float* weights,
                 layer_t* curr_layer,
@@ -38,90 +17,9 @@ void batch_norm(float* inputs,
     nnet_mkl::MklSession* session =
             reinterpret_cast<nnet_mkl::MklSession*>(device->session);
 
-    bool is_fc_output = curr_layer->inputs.height == 1;
-    memory::dims input_dims =
-            is_fc_output
-                    ? get_input_dims_for_fc_output(curr_layer, batch_size)
-                    : get_input_dims_for_conv_output(curr_layer, batch_size);
-    auto input_md =
-            mem_d({ input_dims }, memory::data_type::f32, memory::format::nchw);
-    auto input_mempd = mem_pd(input_md, session->cpu);
-    auto input_memory = memory(input_mempd, inputs);
-
-    // The channel dimension of the input indicates the number of parameter sets
-    // (mean, variance, gamma, beta).
-    int num_sets_weights =
-            is_fc_output ? curr_layer->inputs.rows * curr_layer->inputs.cols
-                         : curr_layer->inputs.height;
-    ARRAY_2D(float, _weights, weights, num_sets_weights);
-
-    memory::dims mean_var_dims = { num_sets_weights };
-    auto mean_md = memory::desc(
-            { mean_var_dims }, memory::data_type::f32, memory::format::x);
-    auto mean_mem_pd = memory::primitive_desc(mean_md, session->cpu);
-    auto mean_memory = memory(mean_mem_pd, &_weights[MEAN][0]);
-
-    memory::dims var_dims = { num_sets_weights };
-    auto var_md = memory::desc(
-            { var_dims }, memory::data_type::f32, memory::format::x);
-    auto var_mem_pd = memory::primitive_desc(var_md, session->cpu);
-    auto var_memory = memory(var_mem_pd, &_weights[VARIANCE][0]);
-
-    // First dimension is gamma, second is beta (the same way that we store them).
-    memory::dims scaleshift_dims = { 2, num_sets_weights };
-    auto scaleshift_md = memory::desc(
-            { scaleshift_dims }, memory::data_type::f32, memory::format::nc);
-    auto scaleshift_mem_pd = memory::primitive_desc(scaleshift_md, session->cpu);
-    auto scaleshift_memory = memory(scaleshift_mem_pd, &_weights[GAMMA][0]);
-
-    auto bn_desc = batch_normalization_forward::desc(
-            prop_kind::forward_inference,
-            input_md,
-            1e-5,
-            use_global_stats | use_scale_shift);
-    auto bn_pd =
-            batch_normalization_forward::primitive_desc(bn_desc, session->cpu);
-
-    memory::dims output_dims = input_dims;
-    auto output_md = mem_d(
-            { output_dims }, memory::data_type::f32, memory::format::nchw);
-    auto output_mem_pd = mem_pd(output_md, session->cpu);
-    auto output_memory = memory(output_mem_pd, results);
-
-    // Don't bother checking if transforms are needed, since the data is
-    // guaranteed to arrive in a suitable format, and batch norm forwards is an
-    // elementwise operation.
-
-    auto bn_mean_memory = mean_memory;
-    if (mem_pd(bn_pd.mean_primitive_desc()) != mean_mem_pd) {
-        bn_mean_memory = memory(bn_pd.mean_primitive_desc());
-        network.emplace_back(reorder(mean_memory, bn_mean_memory));
-    }
-    auto bn_var_memory = var_memory;
-    if (mem_pd(bn_pd.variance_primitive_desc()) != var_mem_pd) {
-        bn_var_memory = memory(bn_pd.variance_primitive_desc());
-        network.emplace_back(reorder(var_memory, bn_var_memory));
-    }
-    auto bn_dst_memory = output_memory;
-    if (mem_pd(bn_pd.dst_primitive_desc()) != output_mem_pd) {
-        bn_dst_memory = memory(bn_pd.dst_primitive_desc());
-        network.emplace_back(reorder(output_memory, bn_dst_memory));
-    }
-    auto bn_weights_memory = scaleshift_memory;
-    if (mem_pd(bn_pd.weights_primitive_desc()) != scaleshift_mem_pd) {
-        bn_weights_memory = memory(bn_pd.weights_primitive_desc());
-        network.emplace_back(reorder(scaleshift_memory, bn_weights_memory));
-    }
-
-    network.emplace_back(
-            batch_normalization_forward(bn_pd,
-                                        primitive::at(input_memory),
-                                        primitive::at(bn_mean_memory),
-                                        primitive::at(bn_var_memory),
-                                        primitive::at(bn_weights_memory),
-                                        output_memory));
-
-    stream(stream::kind::eager).submit(network).wait();
+    BatchNormOp<dtype> batch_norm_op(
+            inputs, weights, results, curr_layer, batch_size, session->cpu);
+    batch_norm_op.run();
 }
 
 }  // namespace nnet_mkl
--- nnet_lib/src/core/mkl/batch_norm.h
+++ nnet_lib/src/core/mkl/batch_norm.h
@@ -8,6 +8,150 @@
 
 namespace nnet_mkl {
 
+template <typename DType>
+class BatchNormOp : public BaseMklOp<DType> {
+   public:
+    // Weights are organized in blocks in this order.
+    enum WeightsIndex {
+        MeanIndex,
+        VarianceIndex,
+        ScaleshiftIndex,  // Gamma, then beta.
+        NumWeightTypes
+    };
+    static constexpr DType kEpsilon = mkl_traits<DType>::to_type(1e-5);
+
+    BatchNormOp(DType* input_buffer,
+                DType* weights_buffer,
+                DType* output_buffer,
+                layer_t* _layer,
+                int _batch_size,
+                mkldnn::engine& engine)
+            : BaseMklOp<DType>(engine), layer(_layer), batch_size(_batch_size) {
+        auto input_mem = create_input_memory(input_buffer);
+        auto mean_mem = create_mean_memory(weights_buffer);
+        auto variance_mem = create_variance_memory(weights_buffer);
+        auto scaleshift_mem = create_scaleshift_memory(weights_buffer);
+        auto output_mem = create_output_memory(output_buffer);
+
+        create_primitive(
+                input_mem, mean_mem, variance_mem, scaleshift_mem, output_mem);
+    }
+
+   protected:
+    // Returns true if the input to BN is the output of an FC layer.
+    bool is_fc_output() {
+        return layer->inputs.height == 1;
+    }
+
+    // Return a mem_dims object for the input, assuming nchw format.
+    mem_dims get_input_dims() {
+        int input_size = layer->inputs.rows * layer->inputs.height *
+                         (layer->inputs.cols + layer->inputs.align_pad);
+        // The size of the batch normalization is indicated by the channel
+        // dimension. Therefore, the output of a FC layer must have all its
+        // dimensionality (except for batch size) compressed into the channel
+        // dimension, with rows = cols = 1. Output of CONV layers can remain
+        // the same.
+        return is_fc_output()
+                       ? mem_dims({ batch_size, input_size, 1, 1 })
+                       : mem_dims({ batch_size, layer->inputs.height,
+                                    layer->inputs.rows, layer->inputs.cols });
+    }
+
+    // Return a mem_dims object for the output, assuming nchw format.
+    mem_dims get_output_dims() {
+        return get_input_dims();
+    }
+
+    // Return the dimensions of the means.
+    mem_dims get_mean_dims() {
+        int num_sets_weights = get_input_dims()[1];
+        return { num_sets_weights };
+    }
+
+    // Return the dimensions of the variances.
+    mem_dims get_variance_dims() {
+        int num_sets_weights = get_input_dims()[1];
+        return { num_sets_weights };
+    }
+
+    // Return the dimensions of the scaleshift weights.
+    mem_dims get_scaleshift_dims() {
+        int num_sets_weights = get_input_dims()[1];
+        return{ 2, num_sets_weights };
+    }
+
+    // Create an input memory primitive from a raw pointer.
+    mem_ref_t create_input_memory(DType* buffer) {
+        return this->create_memory(buffer, get_input_dims(), mem_fmt::nchw);
+    }
+
+    // Create an output memory primitive from a raw pointer.
+    mem_ref_t create_output_memory(DType* buffer) {
+        return this->create_memory(
+                buffer, get_output_dims(), mem_fmt::nchw, true);
+    }
+
+    // Create a memory primitive for the means.
+    mem_ref_t create_mean_memory(DType* buffer) {
+        auto mean_dims = get_mean_dims();
+        return this->create_memory(
+                buffer + MeanIndex * mean_dims[0], mean_dims, mem_fmt::x);
+    }
+
+    // Create a memory primitive for the variances.
+    mem_ref_t create_variance_memory(DType* buffer) {
+        auto variance_dims = get_variance_dims();
+        return this->create_memory(buffer + VarianceIndex * variance_dims[0],
+                                   variance_dims, mem_fmt::x);
+    }
+
+    // Create a memory primitive for the variances.
+    mem_ref_t create_scaleshift_memory(DType* buffer) {
+        auto scaleshift_dims = get_scaleshift_dims();
+        return this->create_memory(
+                buffer + ScaleshiftIndex * scaleshift_dims[1], scaleshift_dims,
+                mem_fmt::nc);
+    }
+
+    // Create a batch norm primitive.
+    mkldnn::primitive& create_primitive(mem_ref_t input,
+                                        mem_ref_t mean,
+                                        mem_ref_t variance,
+                                        mem_ref_t scaleshift,
+                                        mem_ref_t output) {
+        const auto& input_md = input.get_primitive_desc().desc();
+        auto bn_desc = mkldnn::batch_normalization_forward::desc(
+                mkldnn::prop_kind::forward_inference,
+                input_md,
+                kEpsilon,
+                mkldnn::use_global_stats | mkldnn::use_scale_shift);
+        auto bn_pd = mkldnn::batch_normalization_forward::primitive_desc(
+                bn_desc, this->engine);
+
+        mem_ref_t bn_mean = this->reorder_input_if_needed(
+                mean, bn_pd.mean_primitive_desc());
+        mem_ref_t bn_variance = this->reorder_input_if_needed(
+                variance, bn_pd.variance_primitive_desc());
+        mem_ref_t bn_scaleshift = this->reorder_input_if_needed(
+                scaleshift, bn_pd.weights_primitive_desc());
+
+        // Must explicitly cast to primitive::at for batch_norm, or constructor
+        // overload resolution actually picks the wrong constructor.
+        typedef mkldnn::primitive::at at;
+        this->template create_primitive_with_output_reorder<
+                mkldnn::batch_normalization_forward>(
+                bn_pd, output, at(input), at(bn_mean), at(bn_variance),
+                at(bn_scaleshift));
+
+        return this->worklist.back();
+   }
+
+    // The batch norm layer configuration.
+    const layer_t* layer;
+    const int batch_size;
+};
+
 void batch_norm(float* inputs,
                 float* weights,
                 layer_t* curr_layer,
