5704623411612e173442a66fcff0ca08568e20f6
Sam Xi <samxi@seas.harvard.edu>
2017-12-18 21:37:11 -0500

    mkl: Move some activation function op to a BaseMklOp class.
    Change-Id: I02e961be67420db4d198717298036119a063a484

--- nnet_lib/src/arch/nnet_mkl.h
+++ nnet_lib/src/arch/nnet_mkl.h
@@ -13,6 +13,8 @@ using mem_d = mkldnn::memory::desc;
 using mem_pd = mkldnn::memory::primitive_desc;
 using mem_dtype = mkldnn::memory::data_type;
 using mem_fmt = mkldnn::memory::format;
+using mem_dims = mkldnn::memory::dims;
+typedef const mkldnn::memory& mem_ref_t;
 
 // This is the operational datatype used throughout the MKL backend.
 using dtype = float;
@@ -29,6 +31,101 @@ struct mkl_traits<float> {
     // increase precision.
     static constexpr float scaling_factor = 1.0;
     static constexpr float to_type(float value) { return value; }
+};
+
+// The base class for all MKL backend operations in SMAUG.
+template <typename DType>
+class BaseMklOp {
+   public:
+    BaseMklOp(mkldnn::engine& eng) : engine(eng), output_idx(-1) {}
+    virtual ~BaseMklOp() {}
+
+    void run() {
+        mkldnn::stream(mkldnn::stream::kind::eager).submit(worklist).wait();
+    }
+
+    // Return the list of primitives.
+    //
+    // This is useful if execution should be delayed.
+    std::vector<mkldnn::primitive>& get_worklist() { return worklist; }
+
+    const mem_d& get_output_mem_desc() const {
+        return memories.at(output_idx).get_primitive_desc().desc();
+    }
+
+   protected:
+    // Create a memory primitive from an existing buffer.
+    //
+    // A reference to the created memory primitive is returned.
+    //
+    // If @buffer is not NULL, then the memory primitive will use that pointer;
+    // otherwise, it will allocate its own memory.
+    mem_ref_t create_memory(DType* buffer,
+                            mem_dims dims,
+                            mem_fmt fmt,
+                            bool is_output = false) {
+        auto md = mem_d({ dims }, mkl_traits<DType>::dtype, fmt);
+        auto mempd = mem_pd(md, engine);
+        if (buffer)
+            memories.emplace_back(mempd, buffer);
+        else
+            memories.emplace_back(mempd);
+        if (is_output)
+            output_idx = memories.size() - 1;
+        return memories.back();
+    }
+
+    // Create a memory primitive from a primitive descriptor.
+    //
+    // An index to the created memory primitive is returned.
+    mem_ref_t create_memory(mkldnn::memory::primitive_desc pd,
+                            bool is_output = false) {
+        memories.emplace_back(pd);
+        if (is_output)
+            output_idx = memories.size() - 1;
+        return memories.back();
+    }
+
+    // Returns true if the given memory format != target descriptor format.
+    bool needs_reorder(mem_ref_t current_mem,
+                       const mkldnn::memory::primitive_desc& target_desc) {
+        return (mem_pd(target_desc) != current_mem.get_primitive_desc());
+    }
+
+    // Adds a reorder primitive to the worklist if required for this memory.
+    //
+    // The given memory's primitive descriptor is compared against the target
+    // descriptor. If they differ, then a new memory primitive to store the
+    // result of the reorder will be added, and a reorder primitive will be
+    // added to the worklist.
+    //
+    // This is meant to be used for inputs to an operation primitive, because
+    // the reorder is added immediately.
+    mem_ref_t reorder_input_if_needed(
+            mem_ref_t current_mem,
+            const mkldnn::memory::primitive_desc& target_desc) {
+        if (needs_reorder(current_mem, target_desc)) {
+            memories.emplace_back(std::make_shared<mkldnn::memory>(target_desc));
+            worklist.emplace_back(
+                    mkldnn::reorder(current_mem, memories.back()));
+            return memories.back();
+        }
+        return current_mem;
+    }
+
+    // The execution engine.
+    const mkldnn::engine& engine;
+
+    // A list of all the memory objects required. These cannot be destroyed
+    // before the list of primitives has been executed!
+    std::vector<mkldnn::memory> memories;
+
+    // A list of primitives to execute.
+    std::vector<mkldnn::primitive> worklist;
+
+   private:
+    // The index of the memory storing the output.
+    size_t output_idx;
 };
 
 class MklSession {
--- nnet_lib/src/core/mkl/activation_functions.h
+++ nnet_lib/src/core/mkl/activation_functions.h
@@ -8,86 +8,33 @@
 namespace nnet_mkl {
 
 template <typename DType>
-class ActivationFunctionOp {
+class ActivationFunctionOp : public BaseMklOp<DType> {
    public:
-    using mem_index_t = int;
-
-    ActivationFunctionOp(mkldnn::engine& eng) : engine(eng), output_idx(-1) {}
+    ActivationFunctionOp(mkldnn::engine& eng) : BaseMklOp<DType>(eng) {}
     virtual ~ActivationFunctionOp() {}
 
-    // Execute the activation function.
-    void run() {
-        mkldnn::stream(mkldnn::stream::kind::eager).submit(worklist).wait();
-    }
-
-    // Return the list of primitives.
-    //
-    // This is useful if execution should be delayed.
-    std::vector<mkldnn::primitive>& get_worklist() { return worklist; }
-
-    // Return the memory primitive containing the output of this operation.
-    mkldnn::memory& get_output_memory() { return memories.at(output_idx); }
-
    protected:
-    // Create and return an index to a memory primitive.
-    //
-    // If @buffer is not NULL, then the memory primitive will use that pointer;
-    // otherwise, it will allocate its own memory.
-    //
-    // The memory primitive is stored in a list inside this object. The index
-    // corresponds to the location of the primitive in this list. It is
-    // guaranteed to be stable during the lifetime of this Op object.
-    virtual mem_index_t create_memory(DType* buffer,
-                                      int size,
-                                      bool is_output = false) {
-        mkldnn::memory::dims dims = { size };
-        auto md = mem_d({ dims }, mkl_traits<DType>::dtype, mem_fmt::x);
-        auto mempd = mem_pd(md, engine);
-        if (buffer)
-            memories.emplace_back(mempd, buffer);
-        else
-            memories.emplace_back(mempd);
-        mem_index_t retval = memories.size() - 1;
-        if (is_output)
-            output_idx = retval;
-        return retval;
+    mem_ref_t create_memory(DType* buffer, int size, bool is_output = false) {
+        return BaseMklOp<DType>::create_memory(
+                buffer, mem_dims({ size }), mem_fmt::x, is_output);
     }
 
     // Create a return a reference to the operation primitive.
-    virtual mkldnn::eltwise_forward& create_primitive(mkldnn::algorithm alg,
-                                                      mem_index_t input_idx,
-                                                      mem_index_t output_idx,
-                                                      DType alpha = 0,
-                                                      DType beta = 0) {
-        auto& input = memories.at(input_idx);
-        auto& output = memories.at(output_idx);
+    mkldnn::eltwise_forward& create_primitive(mkldnn::algorithm alg,
+                                              mem_ref_t input,
+                                              mem_ref_t output,
+                                              DType alpha = 0,
+                                              DType beta = 0) {
         auto desc =
                 mkldnn::eltwise_forward::desc(mkldnn::prop_kind::forward,
                                               alg,
                                               input.get_primitive_desc().desc(),
                                               alpha,
                                               beta);
-        auto pd = mkldnn::eltwise_forward::primitive_desc(desc, engine);
-        worklist.emplace_back(mkldnn::eltwise_forward(pd, input, output));
-        return static_cast<mkldnn::eltwise_forward&>(worklist.back());
+        auto pd = mkldnn::eltwise_forward::primitive_desc(desc, this->engine);
+        this->worklist.emplace_back(mkldnn::eltwise_forward(pd, input, output));
+        return static_cast<mkldnn::eltwise_forward&>(this->worklist.back());
     }
-
-   protected:
-    // The execution engine.
-    const mkldnn::engine& engine;
-
-    // A list of all the memory objects required. These cannot be destroyed
-    // before the list of primitives has been executed!
-    std::vector<mkldnn::memory> memories;
-
-    // A list of primitives to execute.
-    std::vector<mkldnn::primitive> worklist;
-
-    // The index of the memory storing the output.
-    //
-    // Subsequent operations can use this to directly get a reference to the
-    // memory primitive, instead of creating a new one.
-    mem_index_t output_idx;
 };
 
 class ReluActivationFunctionOp : public ActivationFunctionOp<dtype> {
@@ -201,34 +148,26 @@ class SoftmaxActivationFunctionOp : public ActivationFunctionOp<dtype> {
         create_primitive(input_mem_index, output_mem_index);
     }
 
-    virtual mem_index_t create_memory(dtype* buffer,
-                                      int softmax_size,
-                                      int batch_size,
-                                      bool is_output = false) {
-        mkldnn::memory::dims dims = { batch_size, softmax_size };
-        auto md = mem_d({ dims }, mkl_traits<dtype>::dtype, mem_fmt::nc);
-        auto mempd = mem_pd(md, engine);
-        if (buffer)
-            memories.emplace_back(mempd, buffer);
-        else
-            memories.emplace_back(mempd);
-        mem_index_t retval = memories.size() - 1;
-        if (is_output)
-            output_idx = retval;
-        return retval;
+    mem_ref_t create_memory(dtype* buffer,
+                            int softmax_size,
+                            int batch_size,
+                            bool is_output = false) {
+        return BaseMklOp<dtype>::create_memory(
+                buffer,
+                mem_dims({ batch_size, softmax_size }),
+                mem_fmt::nc,
+                is_output);
     }
 
-    virtual mkldnn::softmax_forward& create_primitive(mem_index_t input_idx,
-                                                      mem_index_t output_idx) {
-        auto& input_memory = memories.at(input_idx);
-        auto& output_memory = memories.at(output_idx);
+    virtual mkldnn::softmax_forward& create_primitive(mem_ref_t input,
+                                                      mem_ref_t output) {
         auto desc = mkldnn::softmax_forward::desc(
                 mkldnn::prop_kind::forward_inference,
-                input_memory.get_primitive_desc().desc(),
+                input.get_primitive_desc().desc(),
                 1);
         auto pd = mkldnn::softmax_forward::primitive_desc(desc, engine);
         worklist.emplace_back(
-                mkldnn::softmax_forward(pd, input_memory, output_memory));
+                mkldnn::softmax_forward(pd, input, output));
         return static_cast<mkldnn::softmax_forward&>(worklist.back());
     }
 
