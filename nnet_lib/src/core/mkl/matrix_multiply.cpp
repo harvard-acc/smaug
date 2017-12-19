24d8030a60cb4fcfd8bd23bc5684409d061f020e
Sam Xi <samxi@seas.harvard.edu>
2017-12-19 13:10:23 -0500

    mkl: Implement InnerProductOp<DType>.
    Change-Id: I7768a1b8c621ae63bc4e3d74e3ed6d1f955f1caa

--- nnet_lib/src/core/mkl/matrix_multiply.cpp
+++ nnet_lib/src/core/mkl/matrix_multiply.cpp
@@ -2,6 +2,7 @@
 
 #include "mkldnn.hpp"
 
+#include "core/mkl/matrix_multiply.h"
 #include "core/nnet_fwd_defs.h"
 #include "arch/nnet_mkl.h"
 
@@ -17,92 +18,9 @@ void matrix_multiply_with_bias(float* inputs,
     std::vector<primitive> network;
     nnet_mkl::MklSession* session =
             reinterpret_cast<nnet_mkl::MklSession*>(device->session);
-
-    // Convert to MKL-DNN dimension descriptors.
-    memory::dims mm_input_dims = { NUM_TEST_CASES,
-                                   curr_layer->inputs.cols };
-    // NOTE: The weights not only need to be transposed, but the actual
-    // dimensions have to be specified as the transpose too.
-    memory::dims mm_weight_dims = { curr_layer->weights.cols,
-                                    curr_layer->weights.rows - 1 };
-    memory::dims mm_bias_dims = { curr_layer->weights.cols };
-    memory::dims mm_output_dims = { NUM_TEST_CASES,
-                                    curr_layer->outputs.cols };
-
-    // Weights are assumed to not be transposed.
-    float* biases = weights +
-             ((curr_layer->weights.rows - 1) * curr_layer->weights.cols);
-
-    // Create memory descriptors.
-    auto user_input_md = mem_d(
-            { mm_input_dims }, memory::data_type::f32, memory::format::nc);
-    auto user_weight_md = mem_d(
-            { mm_weight_dims }, memory::data_type::f32, memory::format::oi);
-    auto user_bias_md = mem_d(
-            { mm_bias_dims }, memory::data_type::f32, memory::format::x);
-    auto user_output_md = mem_d(
-            { mm_output_dims }, memory::data_type::f32, memory::format::nc);
-
-    // Create primitive memory descriptors for user data.
-    auto user_input_memory_descriptor = mem_pd(user_input_md, session->cpu);
-    auto user_weight_memory_descriptor = mem_pd(user_weight_md, session->cpu);
-    auto user_bias_memory_descriptor = mem_pd(user_bias_md, session->cpu);
-    auto user_output_memory_descriptor = mem_pd(user_output_md, session->cpu);
-
-    // Create memory primitives for user input data (input, weights, biases).
-    auto user_input_memory = memory(user_input_memory_descriptor, inputs);
-    auto user_weight_memory = memory(user_weight_memory_descriptor, weights);
-    auto user_bias_memory = memory(user_bias_memory_descriptor, biases);
-
-    // Create memory descriptors for the inner product primitive.
-    auto mm_input_md = mem_d(
-            { mm_input_dims }, memory::data_type::f32, memory::format::any);
-    auto mm_weight_md = mem_d(
-            { mm_weight_dims }, memory::data_type::f32, memory::format::any);
-    auto mm_bias_md = mem_d(
-            { mm_bias_dims }, memory::data_type::f32, memory::format::any);
-    auto mm_output_md = mem_d(
-            { mm_output_dims }, memory::data_type::f32, memory::format::any);
-
-    // Create the inner product primitive descriptor.
-    auto mm_desc = inner_product_forward::desc(prop_kind::forward_inference,
-                                               mm_input_md, mm_weight_md,
-                                               user_bias_md, mm_output_md);
-    auto mm_pd = inner_product_forward::primitive_desc(mm_desc, session->cpu);
-
-    // Check if a data layout transform is required.
-    auto mm_input_memory = user_input_memory;
-    if (mem_pd(mm_pd.src_primitive_desc()) != user_input_memory_descriptor) {
-        mm_input_memory = memory(mm_pd.src_primitive_desc());
-        network.emplace_back(reorder(user_input_memory, mm_input_memory));
-    }
-
-    auto mm_weight_memory = user_weight_memory;
-    if (mem_pd(mm_pd.weights_primitive_desc()) !=
-        user_weight_memory_descriptor) {
-        mm_weight_memory = memory(mm_pd.src_primitive_desc());
-        network.emplace_back(reorder(user_weight_memory, mm_weight_memory));
-    }
-
-    // Create memory primitives for the output.
-    bool output_fmt_matches = (mem_pd(mm_pd.dst_primitive_desc()) !=
-                               user_output_memory_descriptor);
-    auto mm_output_memory =
-            output_fmt_matches ? memory(user_output_memory_descriptor, results)
-                               : memory(mm_pd.dst_primitive_desc());
-
-    // Finally, create the inner product primitive.
-    network.emplace_back(
-            inner_product_forward(mm_pd, mm_input_memory, mm_weight_memory,
-                                  user_bias_memory, mm_output_memory));
-
-    auto user_output_memory = mm_output_memory;
-    if (!output_fmt_matches) {
-        user_output_memory = memory(user_output_memory_descriptor, results);
-        network.emplace_back(reorder(mm_output_memory, user_output_memory));
-    }
-
-    stream(stream::kind::eager).submit(network).wait();
+    InnerProductOp<dtype> inner_product_op(
+            inputs, weights, results, curr_layer, NUM_TEST_CASES, session->cpu);
+    inner_product_op.run();
 }
 
 }  // namespace nnet_mkl
--- nnet_lib/src/core/mkl/matrix_multiply.h
+++ nnet_lib/src/core/mkl/matrix_multiply.h
@@ -5,6 +5,110 @@
 
 namespace nnet_mkl {
 
+template <typename DType>
+class InnerProductOp : public BaseMklOp<DType> {
+   public:
+    InnerProductOp(DType* input_buffer,
+                   DType* weights_buffer,
+                   DType* output_buffer,
+                   layer_t* _layer,
+                   int _batch_size,
+                   mkldnn::engine& engine)
+            : BaseMklOp<DType>(engine), layer(_layer), batch_size(_batch_size) {
+        auto input_mem = create_input_memory(input_buffer);
+        auto weight_mem = create_weight_memory(weights_buffer);
+        auto bias_mem = create_bias_memory(weights_buffer);
+        auto output_mem = create_output_memory(output_buffer);
+
+        create_primitive(input_mem, weight_mem, bias_mem, output_mem);
+    }
+
+   protected:
+    // Return a mem_dims object for the input, assuming nc format.
+    mem_dims get_input_dims() {
+        return { batch_size, layer->inputs.cols };
+    }
+
+    // Return a mem_dims object for the output, assuming nc format.
+    mem_dims get_output_dims() {
+        return { batch_size, layer->outputs.cols };
+    }
+
+    // Return a mem_dims object for the weight, assuming oi format.
+    mem_dims get_weight_dims() {
+        return { layer->weights.cols, layer->weights.rows - 1 };
+    }
+
+    // Return a mem_dims object for the bias, assuming x format.
+    mem_dims get_bias_dims() { return { layer->weights.cols }; }
+
+    // Create an input memory primitive from a raw pointer.
+    //
+    // Returns the index to this primitive.
+    mem_ref_t create_input_memory(DType* buffer) {
+        return this->create_memory(buffer, get_input_dims(), mem_fmt::nc);
+    }
+
+    // Create an output memory primitive from a raw pointer.
+    mem_ref_t create_output_memory(DType* buffer) {
+        return this->create_memory(
+                buffer, get_output_dims(), mem_fmt::nc, true);
+    }
+
+    // Create a weight memory primitive from a raw pointer.
+    mem_ref_t create_weight_memory(DType* buffer) {
+        return this->create_memory(buffer, get_weight_dims(), mem_fmt::oi);
+    }
+
+    // Create a bias memory primitive from a pointer, assuming nchw format.
+    //
+    // The pointer is assumed to point at the beginning of the weights, with
+    // biases stored at the end.
+    mem_ref_t create_bias_memory(DType* weights_buffer) {
+        // Biases are assumed to not be transposed.
+        float* biases = weights_buffer + ((layer->weights.rows - 1) *
+                                          layer->weights.cols);
+        return this->create_memory(biases, get_bias_dims(), mem_fmt::x);
+    }
+
+    // Create an inner product primitive.
+    //
+    // Supply the memory indices for inputs, weights, bias, and outputs.
+    // Optionally, set force_output_format = true to force the output into nchw
+    // format; otherwise, it will simply use whatever format MKL-DNN decides.
+    mkldnn::primitive& create_primitive(mem_ref_t input,
+                                        mem_ref_t weights,
+                                        mem_ref_t bias,
+                                        mem_ref_t output) {
+        mem_dtype dtype = mkl_traits<DType>::dtype;
+        auto mm_input_md = mem_d({ get_input_dims() }, dtype, mem_fmt::any);
+        auto mm_weight_md = mem_d({ get_weight_dims() }, dtype, mem_fmt::any);
+        auto mm_bias_md = mem_d({ get_bias_dims() }, dtype, mem_fmt::x);
+        auto mm_output_md = mem_d({ get_output_dims() }, dtype, mem_fmt::any);
+
+        auto mm_desc = mkldnn::inner_product_forward::desc(
+                mkldnn::prop_kind::forward_inference, mm_input_md, mm_weight_md,
+                mm_bias_md, mm_output_md);
+        auto mm_pd = mkldnn::inner_product_forward::primitive_desc(
+                mm_desc, this->engine);
+
+        auto mm_inputs = this->reorder_input_if_needed(
+                input, mm_pd.src_primitive_desc());
+        auto mm_weights = this->reorder_input_if_needed(
+                weights, mm_pd.weights_primitive_desc());
+
+        this->template create_primitive_with_output_reorder<
+                mkldnn::inner_product_forward>(
+                mm_pd, output, mm_inputs, mm_weights, bias);
+
+        return this->worklist.back();
+    }
+
+    // The inner product layer configuration.
+    const layer_t* layer;
+    const int batch_size;
+};
+
 void matrix_multiply_with_bias(float* inputs,
                                float* weights,
                                layer_t* curr_layer,
