9d8bfd29484f1da1b0f33ac9ec0621397db95d3e
Sam Xi <samxi@seas.harvard.edu>
2017-12-30 22:17:24 -0500

    smiv: Support work division for large FC layers.
    When an FC layer is too large for all the weights to fit into UMEM, the
    weights are divided up into chunks that are individually computed. Each
    chunk is num_inputs x num_chunk_neurons, where num_chunk_neurons must be
    a multiple of 8 (the SMIV vector size). Results are sent back to the
    host after every iteration and aggregated at the end, with some
    reordering required if the batch size is greater than 1.
    
    Change-Id: Iaffba0fc0e04738dfda83837995c40d8a8f99904

--- nnet_lib/src/arch/smiv.cpp
+++ nnet_lib/src/arch/smiv.cpp
@@ -13,6 +13,7 @@
 #include "core/ref/pooling.h"
 #include "core/ref/zeropad.h"
 #include "core/smiv/smiv.h"
+#include "core/smiv/params.h"
 #include "utility/utility.h"
 #include "utility/profiling.h"
 #include "utility/mkl/utility.h"
@@ -67,6 +68,13 @@ typedef struct _conv_cfg_t {
     // Size of the array.
     unsigned num_iterations;
 } conv_cfg_t;
+
+typedef conv_cfg_t fc_cfg_t;
+
+void init_cfg_t(conv_cfg_t* ptr, unsigned num_iterations) {
+    ptr->num_iterations = num_iterations;
+    ptr->iteration = (dims_t*)malloc(sizeof(dims_t) * num_iterations);
+}
 
 // Use the same accelerator id for both the convolutional and FC blocks. This
 // means we will simulate only ONE datapath instead of two, which means that
@@ -198,6 +206,95 @@ void inner_product_layer_acp_hw(float* host_activations,
     }
 }
 
+// Returns true if this inner product layer will require multiple iterations.
+bool inner_product_needs_work_division(layer_t* layers, int lnum) {
+    const unsigned total_weight_bytes = WEIGHT_BYTES(layers, lnum);
+    return total_weight_bytes > UMEM_SIZE;
+}
+
+// Divides the work for a FC layer into several iterations on SMIV.
+//
+// Work division is required when the number of weights exceeds what can be fit
+// on the UMEM. The weight matrix is In x On, where In is the number of input
+// neurons and On is the number of output neurons. Dividing the work means to
+// do the matrix multiply in groups of In x W, where W = On/iterations.
+fc_cfg_t inner_product_divide_work(layer_t* layers, int lnum) {
+    fc_cfg_t fc_cfgs;
+    // TODO: These are not quite the right constraints.
+    const unsigned total_input_bytes =
+            INPUT_BYTES(layers, lnum) / NUM_TEST_CASES;
+    if (total_input_bytes > SPAD_SIZE) {
+        printf("A single input does not fit in the SPAD, which is not "
+               "supported!\n");
+        assert(false);
+    }
+    const unsigned total_output_bytes =
+            OUTPUT_BYTES(layers, lnum) / NUM_TEST_CASES;
+    if (total_output_bytes > SPAD_SIZE) {
+        printf("A single output does not fit in the SPAD, which is not "
+               "supported!\n");
+        assert(false);
+    }
+    if (!inner_product_needs_work_division(layers, lnum)) {
+        // No work division means to return an fc_cfg_t that is holds the
+        // entire weights.
+        init_cfg_t(&fc_cfgs, 1);
+        fc_cfgs.iteration[0] = layers[lnum].weights;
+        return fc_cfgs;
+    }
+    // Divide up the weights. The minimum required work (for now) is an Nx8
+    // strip of weights, where N is the number of hidden neurons.
+    const int num_inputs = layers[lnum].weights.rows;
+    const unsigned num_neurons =
+            layers[lnum].weights.cols + layers[lnum].weights.align_pad;
+    const unsigned minimum_work_size = num_inputs * VECTOR_SIZE * sizeof(float);
+    if (minimum_work_size > UMEM_SIZE) {
+        printf("This weights layer exceeds our current capability to run!\n");
+        assert(false);
+    }
+    const unsigned max_work_units_per_iteration = UMEM_SIZE / minimum_work_size;
+    const unsigned bytes_per_iteration =
+            max_work_units_per_iteration * minimum_work_size;
+    const unsigned num_cols_per_iteration =
+            bytes_per_iteration / num_inputs / sizeof(float);
+    const unsigned total_weight_bytes = WEIGHT_BYTES(layers, lnum);
+    const unsigned num_iterations =
+            ceil(((float)total_weight_bytes) / bytes_per_iteration);
+
+    init_cfg_t(&fc_cfgs, num_iterations);
+    unsigned num_cols_remaining = num_neurons;
+    for (unsigned i = 0; i < num_iterations; i++) {
+        int num_cols_this_iter =
+                min2(num_cols_remaining, num_cols_per_iteration);
+        // We can ignore align_pad here because num_neurons has already
+        // accounted for the original required padding.
+        fc_cfgs.iteration[i] = (dims_t){ num_inputs, num_cols_this_iter, 1, 0 };
+        num_cols_remaining -= num_cols_this_iter;
+    }
+    return fc_cfgs;
+}
+
+// Copy the weights section from (0, start_col) to (num_rows, start_col +
+// num_cols). This will include the biases for that section.
+//
+// TODO: Can we avoid this copy by using a really large align_pad?
+void copy_weights_block(float* host_weights,
+                        layer_t* layers,
+                        int lnum,
+                        int start_col,
+                        int num_cols,
+                        float* weights_buffer) {
+    int num_rows = layers[lnum].weights.rows;
+    int num_total_cols =
+            layers[lnum].weights.cols + layers[lnum].weights.align_pad;
+    ARRAY_2D(float, _weights, host_weights, num_total_cols);
+    for (int r = 0; r < num_rows; r++) {
+        memcpy(weights_buffer + r * num_cols,
+               &_weights[r][start_col],
+               num_cols * sizeof(float));
+    }
+}
+
 result_buf inner_product_layer(float* host_activations,
                                float* host_weights,
                                layer_t* layers,
@@ -214,32 +311,160 @@ result_buf inner_product_layer(float* host_activations,
     }
     float* host_weights_layer =
             host_weights + get_weights_loc_for_layer(layers, lnum);
-    PRINT_MSG_V("Weights:\n");
-    PRINT_DEBUG_V(host_weights_layer, layers[lnum].weights.rows,
-                  layers[lnum].weights.cols,
-                  layers[lnum].weights.cols + layers[lnum].weights.align_pad);
 
-    size_t weight_bytes = WEIGHT_BYTES(layers, lnum);
-    assert(weight_bytes <= UMEM_SIZE && "Weights must fit on the UMEM!");
+    PRINT_MSG("Weights:\n");
+    PRINT_DEBUG(host_weights_layer,
+                layers[lnum].weights.rows,
+                layers[lnum].weights.cols,
+                layers[lnum].weights.cols + layers[lnum].weights.align_pad);
+
+    fc_cfg_t fc_cfgs = inner_product_divide_work(layers, lnum);
+    bool needs_multiple_iter = (fc_cfgs.num_iterations > 1);
 
     MAP_ARRAY(kInnerProductHw, host_activations, INPUT_BYTES(layers, lnum));
-    MAP_ARRAY_TO_ACCEL(kInnerProductHw, "host_weights", host_weights_layer,
-                       WEIGHT_BYTES(layers, lnum));
 
-    // If the result is to be in g_spad1, then the input is in g_spad0.
-    bool input_in_spad0 = (current_result_loc == g_spad1);
-    bool use_acp_offload = (device->cpu_activation_func_offload == IO_ACP);
-    if (use_acp_offload) {
-        MAP_ARRAY_TO_ACCEL(kInnerProductHw, "acp_result", host_result,
-                           OUTPUT_BYTES(layers, lnum));
-        INVOKE_KERNEL_PROF(kInnerProductHw, inner_product_layer_acp_hw,
-            host_activations, host_weights_layer, host_result, g_umem, g_spad0,
-            g_spad1, layers, lnum, input_in_spad0);
+    // Holds a contiguous column of weights and the partial results. If work
+    // division is required, then each iteration's chunk of weights is copied
+    // into the buffer; otherwise, we just use the original weights and results
+    // buffers.
+    float* host_weights_buffer;
+    float* host_results_buffer;
+    const size_t weights_buffer_size =
+            (fc_cfgs.iteration[0].cols + fc_cfgs.iteration[0].align_pad) *
+            fc_cfgs.iteration[0].rows * sizeof(float);
+    if (needs_multiple_iter) {
+        host_weights_buffer = (float*)malloc_aligned(weights_buffer_size);
     } else {
-        MAP_ARRAY(kInnerProductHw, host_result, OUTPUT_BYTES(layers, lnum));
-        INVOKE_KERNEL_PROF(kInnerProductHw, inner_product_layer_hw,
-            host_activations, host_weights_layer, g_umem, g_spad0, g_spad1,
-            layers, lnum, input_in_spad0, host_result);
+        host_weights_buffer = host_weights_layer;
+    }
+    if (NUM_TEST_CASES > 1 && needs_multiple_iter) {
+        host_results_buffer =
+                (float*)malloc_aligned(OUTPUT_BYTES(layers, lnum));
+    } else {
+        host_results_buffer = host_result;
+    }
+
+    int current_col = 0;
+    float* current_result = host_results_buffer;
+    for (unsigned it = 0; it < fc_cfgs.num_iterations; it++) {
+        layer_t partial_layer = layers[lnum];
+        partial_layer.weights = fc_cfgs.iteration[it];
+        partial_layer.outputs.cols = fc_cfgs.iteration[it].cols;
+        PRINT_MSG("FC iteration %d: weights %dx%d\n",
+                    it,
+                    partial_layer.weights.rows,
+                    partial_layer.weights.cols);
+
+        if (needs_multiple_iter) {
+            copy_weights_block(host_weights_layer,
+                               layers,
+                               lnum,
+                               current_col,
+                               fc_cfgs.iteration[it].cols +
+                                       fc_cfgs.iteration[it].align_pad,
+                               host_weights_buffer);
+
+            PRINT_DEBUG_V(host_weights_buffer,
+                          fc_cfgs.iteration[it].rows,
+                          fc_cfgs.iteration[it].cols,
+                          fc_cfgs.iteration[it].cols +
+                                  fc_cfgs.iteration[it].align_pad);
+        }
+
+        MAP_ARRAY_TO_ACCEL(kInnerProductHw,
+                           "host_weights",
+                           host_weights_buffer,
+                           weights_buffer_size);
+
+        size_t result_size = NUM_TEST_CASES * partial_layer.outputs.rows *
+                             partial_layer.outputs.cols;
+
+        // If the result is to be in g_spad1, then the input is in g_spad0.
+        bool input_in_spad0 = (current_result_loc == g_spad1);
+        bool use_acp_offload = (device->cpu_activation_func_offload == IO_ACP);
+        if (use_acp_offload) {
+            MAP_ARRAY_TO_ACCEL(kInnerProductHw,
+                               "acp_result",
+                               current_result,
+                               result_size * sizeof(float));
+            INVOKE_KERNEL_PROF(kInnerProductHw,
+                               inner_product_layer_acp_hw,
+                               host_activations,
+                               host_weights_buffer,
+                               current_result,
+                               g_umem,
+                               g_spad0,
+                               g_spad1,
+                               &partial_layer,
+                               0,
+                               input_in_spad0);
+        } else {
+            MAP_ARRAY_TO_ACCEL(kInnerProductHw,
+                               "host_result",
+                               current_result,
+                               result_size * sizeof(float));
+            INVOKE_KERNEL_PROF(kInnerProductHw,
+                               inner_product_layer_hw,
+                               host_activations,
+                               host_weights_buffer,
+                               g_umem,
+                               g_spad0,
+                               g_spad1,
+                               &partial_layer,
+                               0,
+                               input_in_spad0,
+                               current_result);
+        }
+        PRINT_MSG_V("Partial results:\n");
+        PRINT_DEBUG_V(
+                current_result,
+                NUM_TEST_CASES,
+                fc_cfgs.iteration[it].cols,
+                fc_cfgs.iteration[it].cols + fc_cfgs.iteration[it].align_pad);
+
+        current_col += fc_cfgs.iteration[it].cols;
+        current_result += result_size;
+    }
+
+    // Fix up the results if required.
+    //
+    // The desired result looks like (for batch size 2):
+    //
+    // [ input 1, iter 1 results ] [ input 1, iter 2 results ] ...
+    // [ input 1, iter 1 results ] [ input 1, iter 2 results ] ...
+    //
+    // But, when the batch size > 1 and multiple iterations are needed, the
+    // results buffer will end up looking like this:
+    //
+    // [ input 1, iter 1 results ] [ input 2, iter 1 results ] ...
+    // [ input 1, iter 2 results ] [ input 2, iter 2 results ] ...
+    //
+    // This routine reorders the results buffer and stores the result into the
+    // final result array (host_result).
+    if (NUM_TEST_CASES > 1 && needs_multiple_iter) {
+        int output_size =
+                layers[lnum].outputs.cols + layers[lnum].outputs.align_pad;
+        ARRAY_2D(float, _host_results, host_result, output_size);  // dst buffer.
+        current_result = host_results_buffer;  // temporary buffer.
+        int curr_col = 0;
+        for (unsigned it = 0; it < fc_cfgs.num_iterations; it++) {
+            int it_output_size = (fc_cfgs.iteration[it].cols +
+                                  fc_cfgs.iteration[it].align_pad);
+            for (int tc = 0; tc < NUM_TEST_CASES; tc++) {
+                memcpy(&_host_results[tc][curr_col],
+                       current_result,
+                       it_output_size * sizeof(float));
+                current_result += it_output_size;
+            }
+            curr_col += it_output_size;
+        }
+    }
+
+    if (needs_multiple_iter) {
+        free(host_weights_buffer);
+    }
+    if (NUM_TEST_CASES > 1 && needs_multiple_iter) {
+        free(host_results_buffer);
     }
 
     return host_result;
@@ -718,47 +943,54 @@ result_buf run_layer(float* activations,
 // DMA if we need to send data back to the CPU.
 void set_dma_requirements(network_t* network, device_t* device) {
     for (int layer_num = 0; layer_num < network->depth; layer_num++) {
+        layer_t* curr_layer = &network->layers[layer_num];
+
         // The input layer is easy.
         if (layer_num == 0) {
-            network->layers[layer_num].input_req = IO_NONE;
-            network->layers[layer_num].output_req = IO_DMA;
+            curr_layer->input_req = IO_NONE;
+            curr_layer->output_req = IO_DMA;
             continue;
         }
+
+        layer_t* prev_layer = &network->layers[layer_num - 1];
+        layer_t* next_layer = &network->layers[layer_num + 1];
 #if DEBUG_LEVEL > 0
         // When debugging, if we don't DMA the results back, we won't be able
         // to see what's happening.
-        network->layers[layer_num].output_req = IO_DMA;
+        curr_layer->output_req = IO_DMA;
 #else
         // First, determine if we need to dma store the output.
         if (layer_num == network->depth - 1 ||
             // All these activation functions are unsupported.
-            network->layers[layer_num].activation == LRELU ||
-            network->layers[layer_num].activation == ELU ||
-            network->layers[layer_num].activation == SELU ||
-            network->layers[layer_num].activation == TANH ||
-            network->layers[layer_num].activation == SIGMOID ||
-            network->layers[layer_num].activation == SOFTMAX ||
+            curr_layer->activation == LRELU ||
+            curr_layer->activation == ELU ||
+            curr_layer->activation == SELU ||
+            curr_layer->activation == TANH ||
+            curr_layer->activation == SIGMOID ||
+            curr_layer->activation == SOFTMAX ||
             // If we disabled HW activation functions but an activation
             // function is necessary, we need to DMA.
             (!device->use_hw_activation_func &&
-             network->layers[layer_num].activation != NO_ACTIVATION) ||
-            network->layers[layer_num].type == POOLING ||
-            network->layers[layer_num].type == BATCH_NORM ||
+             curr_layer->activation != NO_ACTIVATION) ||
+            curr_layer->type == POOLING ||
             // For now, conv layers also do not support local caching.
-            network->layers[layer_num].type == CONV ||
-            network->layers[layer_num + 1].type == BATCH_NORM ||
-            network->layers[layer_num + 1].type == POOLING) {
-            network->layers[layer_num].output_req = IO_DMA;
+            curr_layer->type == CONV ||
+            curr_layer->type == BATCH_NORM ||
+            next_layer->type == BATCH_NORM ||
+            next_layer->type == POOLING ||
+            // If the FC layer needs work division, we can't locally cache.
+            (curr_layer->type == FC &&
+             inner_product_needs_work_division(network->layers, layer_num))) {
+            curr_layer->output_req = IO_DMA;
         } else {
-            network->layers[layer_num].output_req = IO_NONE;
+            curr_layer->output_req = IO_NONE;
         }
-        if(network->layers[layer_num].input_preprocessing == FLATTEN)
-            network->layers[layer_num - 1].output_req = IO_DMA;
+        if (curr_layer->input_preprocessing == FLATTEN)
+            prev_layer->output_req = IO_DMA;
 #endif
         // Whether we need to load the input on this layer is just whether we
         // had to store the outputs in the previous layer.
-        network->layers[layer_num].input_req =
-                network->layers[layer_num - 1].output_req;
+        curr_layer->input_req = prev_layer->output_req;
     }
 
     for (int layer_num = 0; layer_num < network->depth; layer_num++) {
