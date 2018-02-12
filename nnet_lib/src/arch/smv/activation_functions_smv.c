b818feb104ccba1efbb535bf9ecb26c2b1cac384
Sam Xi <samxi@seas.harvard.edu>
2018-02-12 13:45:23 -0500

    smv: Add SIMD wrapper for activation functions.
    Change-Id: I015afa8054e066eef024b41b8bc340063b50dafa

--- nnet_lib/src/arch/smv/activation_functions_smv.c
+++ nnet_lib/src/arch/smv/activation_functions_smv.c
@@ -5,5 +5,15 @@ void smv_activation_fun(float* activations,
                         int batch_size,
                         int input_size,
                         activation_type activation) {
+#ifdef ENABLE_SIMD_IMPL
+    int num_vectors = input_size / VECTOR_SIZE;
+    VEC_ARRAY_1D(v8fp_t, _activations, activations);
+    activation_vector:
+    for (int i = 0; i < num_vectors; i++) {
+        _activations[i] =
+                activation_fun_simd(_activations[i], activation);
+    }
+#else
     activation_fun(activations, batch_size, input_size, activation);
+#endif
 }
