b9c9a07de4a4a4f784743371d1cf694b6b8ac374
Sam Xi <samxi@seas.harvard.edu>
2018-05-20 18:16:35 -0400

    operators: Add reference inner product operator and test.
    Change-Id: I68483d234a5bf734667aba4911b67c62ba20d87a

---
+++ nnet_lib/src/operators/ref/ref_inner_product_op.cpp
@@ -0,0 +1,59 @@
+#include "core/backend.h"
+#include "operators/common.h"
+#include "operators/inner_product_op.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ref_inner_product_f32_nc(float* a,
+                              float* b,
+                              float* c,
+                              int a_height,
+                              int a_width,
+                              int b_width,
+                              int a_pad,
+                              int b_pad,
+                              int c_pad) {
+
+    ARRAY_2D(float, _a, a, a_width + a_pad);
+    ARRAY_2D(float, _b, b, b_width + b_pad);
+    ARRAY_2D(float, _c, c, b_width + c_pad);
+
+    for (int i = 0; i < a_height; i++) {
+        for (int j = 0; j < b_width; j++) {
+            float result = 0;
+            for (int k = 0; k < a_width; k++) {
+                float a_val = _a[i][k];
+                float b_val = _b[k][j];
+                result += a_val * b_val;
+            }
+            _c[i][j] = result;
+        }
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+namespace smaug {
+
+template <>
+void InnerProductOp<ReferenceBackend>::run() {
+    auto input = getInput<ReferenceBackend>(Inputs);
+    auto weights = getInput<ReferenceBackend>(Weights);
+    auto output = getOutput<ReferenceBackend>(Outputs);
+    const TensorShape& inputShape = input->getShape();
+    const TensorShape& weightShape = weights->getShape();
+    const TensorShape& outputShape = output->getShape();
+    assert(inputShape.getLayout() == DataLayout::NC);
+    assert(weightShape.getLayout() == DataLayout::NC);
+    assert(outputShape.getLayout() == DataLayout::NC);
+
+    ref_inner_product_f32_nc(input->data<float>(), weights->data<float>(),
+                             output->data<float>(), inputShape[0],
+                             inputShape[1], weightShape[1], 0, 0, 0);
+}
+
+}  // namespace smaug
---
+++ nnet_lib/src/operators/ref/ref_inner_product_op_test.cpp
@@ -0,0 +1,95 @@
+#include "catch.hpp"
+#include "core/backend.h"
+#include "core/tensor.h"
+#include "core/smaug_test.h"
+#include "operators/inner_product_op.h"
+
+using namespace smaug;
+
+TEST_CASE_METHOD(SmaugTest, "Reference inner product operator", "[refop]") {
+    auto matMulOp = new InnerProductOp<ReferenceBackend>("matmul", workspace());
+    TensorShape inputShape({ 1, 10 }, DataLayout::NC);
+    Tensor<ReferenceBackend>* input = new Tensor<ReferenceBackend>(
+            "input", inputShape);
+    input->allocateStorage<float>();
+    // Input data looks like:
+    input->fillData<float>({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+    workspace()->addTensor(input);
+    matMulOp->setInput(input, 0);
+    SECTION("10x10, constant weights per neuron") {
+        matMulOp->setNumOutputs(10);
+        matMulOp->createAllTensors();
+        allocateAllTensors<float, ReferenceBackend>(matMulOp);
+        auto weightsTensor = matMulOp->getInput<ReferenceBackend>(1);
+        weightsTensor->fillData<float>({
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        });
+
+        matMulOp->run();
+
+        // Expected output (with zero padding):
+        //
+        // (1...10) * 1 = 55
+        // (1...10) * 2 = 110
+        // (1...10) * 3 = 165
+        // (1...10) * 4 = 220
+        // (1...10) * 5 = 275
+        // (1...10) * 6 = 330
+        // (1...10) * 7 = 385
+        // (1...10) * 8 = 440
+        // (1...10) * 9 = 495
+        // (1...10) * 10 = 550
+        std::vector<float> expectedValues{ 55,  110, 165, 220, 275,
+                                           330, 385, 440, 495, 550 };
+        auto outputsTensor = matMulOp->getOutput<ReferenceBackend>(0);
+        verifyOutputs(outputsTensor, expectedValues);
+    }
+
+    SECTION("10x10, distinct weights per neuron") {
+        matMulOp->setNumOutputs(10);
+        matMulOp->createAllTensors();
+        allocateAllTensors<float, ReferenceBackend>(matMulOp);
+        auto weightsTensor = matMulOp->getInput<ReferenceBackend>(1);
+        weightsTensor->fillData<float>({
+            1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+            2,  3,  4,  5,  6,  7,  8,  9,  10, 11,
+            3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+            4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+            5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
+            6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
+            7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+            8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
+            9,  10, 11, 12, 13, 14, 15, 16, 17, 18,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+        });
+
+        matMulOp->run();
+
+        // Expected output:
+        //
+        // 1*1 + 2*2 + 3*3 + ... 10*10 = 385
+        // 1*2 + 2*3 + 3*4 + ... 10*11 = 385 + (1+...+10) = 385+55 = 440
+        // 1*3 + 2*4 + 3*4 + ... 10*11 = 440 + 55 = 495
+        // 1*4 + 2*3 + 3*4 + ... 10*11 = 550
+        // 1*5 + 2*3 + 3*4 + ... 10*11 = 605
+        // 1*6 + 2*3 + 3*4 + ... 10*11 = 660
+        // 1*7 + 2*3 + 3*4 + ... 10*11 = 715
+        // 1*8 + 2*3 + 3*4 + ... 10*11 = 770
+        // 1*9 + 2*3 + 3*4 + ... 10*11 = 825
+        // 1*10 + 2*3 + 3*4 + ... 10*11 = 880
+        // 385 440 495 550 605 660 715 770 825 880
+        std::vector<float> expectedValues{ 385, 440, 495, 550, 605,
+                                           660, 715, 770, 825, 880 };
+        auto outputsTensor = matMulOp->getOutput<ReferenceBackend>(0);
+        verifyOutputs(outputsTensor, expectedValues);
+    }
+}
