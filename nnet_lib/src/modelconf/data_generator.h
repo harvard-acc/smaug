a6d6a7c62b3b39a6a634c7c3c99714379118f5a1
Sam Xi <samxi@seas.harvard.edu>
2018-07-09 15:20:04 -0400

    modelconf: Generate random data invariant to data layout.
    In the past, a randomly generated tensor in NCHW would physically look
    the same but logically be different from one in NHWC. This makes the
    logical output of each layer of the network dependent on layout, which
    is obviously not desirable. Now, regardless of data layout, randomly
    generated tensors will be logically consistent, in that they will always
    represent the same "image" or "input", even if the underlying storage
    order is different.
    
    Change-Id: I217f52638aa267f92dfc19eded73f50355958bf4

--- nnet_lib/src/modelconf/data_generator.h
+++ nnet_lib/src/modelconf/data_generator.h
@@ -98,13 +98,56 @@ class GaussianDataGenerator : public DataGenerator<DType> {
     DType savedValue;
 };
 
+/* Generate a random tensor invariant to data layout.
+ *
+ * Invariance means that the data logically is the same across different
+ * physical layouts. For example, if the tensor argument is in NHWC, then the
+ * random tensor generated would be equivalent to calling this function with an
+ * NCHW input tensor followed by a reorder operation to NHWC.
+ */
+template <typename DType, typename Backend>
+void generateConsistentRandomTensor(Tensor<Backend>* tensor,
+                                    DataGenerator<DType>* generator) {
+    const TensorShape& shape = tensor->getShape();
+    TensorIndexIterator index = tensor->startIndex();
+    DType* ptr = tensor->template allocateStorage<DType>();
+    if (shape.getLayout() == DataLayout::NCHW) {
+        int N = shape[0];
+        int C = shape[1];
+        int H = shape[2];
+        int W = shape[3];
+        for (int n = 0; n < N; n++)
+            for (int c = 0; c < C; c++)
+                for (int h = 0; h < H; h++)
+                    for (int w = 0; w < W; w++)
+                        ptr[index(n, c, h, w)] = generator->next();
+    } else if (shape.getLayout() == DataLayout::NHWC) {
+        int N = shape[0];
+        int H = shape[1];
+        int W = shape[2];
+        int C = shape[3];
+        for (int n = 0; n < N; n++)
+            for (int c = 0; c < C; c++)
+                for (int h = 0; h < H; h++)
+                    for (int w = 0; w < W; w++)
+                        ptr[index(n, h, w, c)] = generator->next();
+    } else if (shape.getLayout() == DataLayout::NC) {
+        int N = shape[0];
+        int C = shape[1];
+        for (int n = 0; n < N; n++)
+            for (int c = 0; c < C; c++)
+                ptr[index(n, c)] = generator->next();
+    } else if (shape.getLayout() == DataLayout::X) {
+        // If it's X, then it can be any format, which means we don't know what
+        // layout to target.
+        assert(false && "Cannot generate random data if data layout is X!");
+    }
+}
+
 template <typename DType, typename Backend>
 void generateRandomTensor(Tensor<Backend>* tensor,
                           DataGenerator<DType>* generator) {
-    DType* ptr = tensor->template allocateStorage<DType>();
-    for (auto idx = tensor->startIndex(); !idx.end(); ++idx) {
-        ptr[idx] = generator->next();
-    }
+    generateConsistentRandomTensor<DType, Backend>(tensor, generator);
 }
 
 template <typename DType, typename Backend>
