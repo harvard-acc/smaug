34a17c0d678ce1d94206a4749e8af347482a3f2e
Yuan Yao <yuanyao@seas.harvard.edu>
2019-04-29 18:16:42 -0400

    ref: Add reference NHWC implementations for the pooling operator.
    This adds reference NHWC implementations for max and average pooling
    operators. These will be useful for the SMV tests.
    
    Change-Id: I250d6eba476bd08d6877f048910323d95b3c6f04

--- nnet_lib/src/operators/ref/ref_pooling_op.cpp
+++ nnet_lib/src/operators/ref/ref_pooling_op.cpp
@@ -70,6 +70,68 @@ void ref_max_pooling_nchw_treemax(float* input,
     }
 }
 
+void ref_max_pooling_nhwc_treemax(float* input,
+                                  float* result,
+                                  int img_num,
+                                  int img_chans,
+                                  int img_rows,
+                                  int img_cols,
+                                  int img_pad,
+                                  int res_rows,
+                                  int res_cols,
+                                  int res_pad,
+                                  int pool_row_size,
+                                  int pool_col_size,
+                                  int pool_row_stride,
+                                  int pool_col_stride) {
+    int total_pool_size = pool_row_size * pool_col_size;
+    float elems[total_pool_size];
+    int elem_idx;
+
+    ARRAY_4D(float, _input, input, img_rows, img_cols, img_chans + img_pad);
+    ARRAY_4D(float, _result, result, res_rows, res_cols, img_chans + res_pad);
+
+    maxpool_input_num:
+    for (int img = 0; img < img_num; img++) {
+        maxpool_input_height:
+        for (int h = 0; h < img_chans; h++) {
+            int oi = 0;
+            maxpool_input_rows:
+            for (int i = 0; i < img_rows; i += pool_row_stride) {
+                int oj = 0;
+                maxpool_input_cols:
+                for (int j = 0; j < img_cols; j += pool_col_stride) {
+                    elem_idx = 0;
+                    maxpool_tree_outer:
+                    // Iterate over the pooling field.
+                    for (int k = 0; k < pool_row_size; k++) {
+                        maxpool_tree_inner:
+                        for (int l = 0; l < pool_col_size; l++) {
+                            elems[elem_idx] = _input[img][i+k][j+l][h];
+                            elem_idx++;
+                        }
+                    }
+
+                    float curr_max = 0;
+                    if (total_pool_size == 4)
+                        curr_max = max4(elems[0], elems[1], elems[2], elems[3]);
+                    else if (total_pool_size == 9)
+                        curr_max = max9(elems[0], elems[1], elems[2], elems[3],
+                                        elems[4], elems[5], elems[6], elems[7],
+                                        elems[8]);
+                    else
+                        assert(false && "Unsupported pooling size!");
+
+                    _result[img][oi][oj][h] = curr_max;
+                    oj++;
+                }
+                oi++;
+                oj = 0;
+            }
+        }
+    }
+}
+
 void ref_max_pooling_nchw_itermax(float* input,
                                   float* result,
                                   int img_num,
@@ -108,6 +170,53 @@ void ref_max_pooling_nchw_itermax(float* input,
                     }
 
                     _result[img][h][oi][oj] = curr_max;
+                    oj++;
+                }
+                oi++;
+                oj = 0;
+            }
+        }
+    }
+}
+
+void ref_max_pooling_nhwc_itermax(float* input,
+                                  float* result,
+                                  int img_num,
+                                  int img_chans,
+                                  int img_rows,
+                                  int img_cols,
+                                  int img_pad,
+                                  int res_rows,
+                                  int res_cols,
+                                  int res_pad,
+                                  int pool_row_size,
+                                  int pool_col_size,
+                                  int pool_row_stride,
+                                  int pool_col_stride) {
+    ARRAY_4D(float, _input, input, img_rows, img_cols, img_chans + img_pad);
+    ARRAY_4D(float, _result, result, res_rows, res_cols, img_chans + res_pad);
+
+    maxpool_input_num:
+    for (int img = 0; img < img_num; img++) {
+        maxpool_input_height:
+        for (int h = 0; h < img_chans; h++) {
+            int oi = 0;
+            maxpool_input_rows:
+            for (int i = 0; i < img_rows; i += pool_row_stride) {
+                int oj = 0;
+                maxpool_input_cols:
+                for (int j = 0; j < img_cols; j += pool_col_stride) {
+                    float curr_max = -FLT_MAX;
+                    maxpool_iter_outer:
+                    for (int k = 0; k < pool_row_size; k++) {
+                        maxpool_iter_inner:
+                        for (int l = 0; l < pool_col_size; l++) {
+                            float in_val = _input[img][i+k][j+l][h];
+                            curr_max = max2(in_val, curr_max);
+                        }
+                    }
+
+                    _result[img][oi][oj][h] = curr_max;
                     oj++;
                 }
                 oi++;
@@ -163,6 +272,54 @@ void ref_avg_pooling_nchw(float* input,
         }
     }
 }
+
+void ref_avg_pooling_nhwc(float* input,
+                          float* result,
+                          int img_num,
+                          int img_chans,
+                          int img_rows,
+                          int img_cols,
+                          int img_pad,
+                          int res_rows,
+                          int res_cols,
+                          int res_pad,
+                          int pool_row_size,
+                          int pool_col_size,
+                          int pool_row_stride,
+                          int pool_col_stride) {
+    ARRAY_4D(float, _input, input, img_rows, img_cols, img_chans + img_pad);
+    ARRAY_4D(float, _result, result, res_rows, res_cols, img_chans + res_pad);
+    float recip_total_size = 1.0 / (pool_row_size * pool_col_size);
+
+    maxpool_input_num:
+    for (int img = 0; img < img_num; img++) {
+        maxpool_input_height:
+        for (int h = 0; h < img_chans; h++) {
+            int oi = 0;
+            maxpool_input_rows:
+            for (int i = 0; i < img_rows; i += pool_row_stride) {
+                int oj = 0;
+                maxpool_input_cols:
+                for (int j = 0; j < img_cols; j += pool_col_stride) {
+                    float curr_sum = 0;
+                    avgpool_iter_outer:
+                    for (int k = 0; k < pool_row_size; k++) {
+                        avgpool_iter_inner:
+                        for (int l = 0; l < pool_col_size; l++) {
+                            curr_sum += _input[img][i+k][j+l][h];
+                        }
+                    }
+
+                    _result[img][oi][oj][h] = curr_sum * recip_total_size;
+                    oj++;
+                }
+                oi++;
+                oj = 0;
+            }
+        }
+    }
+}
+
 #ifdef __cplusplus
 }
 #endif
@@ -175,12 +332,13 @@ void MaxPoolingOp<ReferenceBackend>::run() {
     auto output = getOutput(Outputs);
     const TensorShape& inputShape = input->getShape();
     const TensorShape& outputShape = output->getShape();
-    assert(inputShape.getLayout() == DataLayout::NCHW);
-    assert(outputShape.getLayout() == DataLayout::NCHW);
 
+    bool isNCHW = input->getShape().getLayout() == NCHW;
     bool useTreeMax = (poolingRowSize <= 3 && poolingRowSize == poolingColSize);
-    auto func = useTreeMax ? ref_max_pooling_nchw_treemax
-                           : ref_max_pooling_nchw_itermax;
+    auto func = isNCHW ? (useTreeMax ? ref_max_pooling_nchw_treemax
+                                     : ref_max_pooling_nchw_itermax)
+                       : (useTreeMax ? ref_max_pooling_nhwc_treemax
+                                     : ref_max_pooling_nhwc_itermax);
     int poolRowSize, poolColSize, poolRowStride, poolColStride;
     std::tie(poolRowSize, poolColSize) = getPoolingSize();
     std::tie(poolRowStride, poolColStride) = getPoolingStride();
@@ -191,11 +349,14 @@ void MaxPoolingOp<ReferenceBackend>::run() {
                     inputShape.storageSize() * sizeof(float));
     mapArrayToAccel(ref::kPoolingHw, "result", outputData,
                     outputShape.storageSize() * sizeof(float));
+    int rowIdx = isNCHW ? 2 : 1;
+    int colIdx = isNCHW ? 3 : 2;
+    int chanIdx = isNCHW ? 1 : 3;
     invokeKernel(ref::kPoolingHw, func, inputData, outputData, inputShape[0],
-                 inputShape[1], inputShape[2], inputShape[3],
-                 inputShape.getPadding(3), outputShape[2], outputShape[3],
-                 outputShape.getPadding(3), poolRowSize, poolColSize,
-                 poolRowStride, poolColStride);
+                 inputShape[chanIdx], inputShape[rowIdx], inputShape[colIdx],
+                 inputShape.getPadding(3), outputShape[rowIdx],
+                 outputShape[colIdx], outputShape.getPadding(3), poolRowSize,
+                 poolColSize, poolRowStride, poolColStride);
 }
 
 template <>
@@ -204,9 +365,9 @@ void AvgPoolingOp<ReferenceBackend>::run() {
     auto output = getOutput(Outputs);
     const TensorShape& inputShape = input->getShape();
     const TensorShape& outputShape = output->getShape();
-    assert(inputShape.getLayout() == DataLayout::NCHW);
-    assert(outputShape.getLayout() == DataLayout::NCHW);
 
+    bool isNCHW = input->getShape().getLayout() == NCHW;
+    auto func = isNCHW ? ref_avg_pooling_nchw : ref_avg_pooling_nhwc;
     int poolRowSize, poolColSize, poolRowStride, poolColStride;
     std::tie(poolRowSize, poolColSize) = getPoolingSize();
     std::tie(poolRowStride, poolColStride) = getPoolingStride();
@@ -217,11 +378,14 @@ void AvgPoolingOp<ReferenceBackend>::run() {
                     inputShape.storageSize() * sizeof(float));
     mapArrayToAccel(ref::kPoolingHw, "result", outputData,
                     outputShape.storageSize() * sizeof(float));
-    invokeKernel(ref::kPoolingHw, ref_avg_pooling_nchw, inputData, outputData,
-                 inputShape[0], inputShape[1], inputShape[2], inputShape[3],
-                 inputShape.getPadding(3), outputShape[2], outputShape[3],
-                 outputShape.getPadding(3), poolRowSize, poolColSize,
-                 poolRowStride, poolColStride);
+    int rowIdx = isNCHW ? 2 : 1;
+    int colIdx = isNCHW ? 3 : 2;
+    int chanIdx = isNCHW ? 1 : 3;
+    invokeKernel(ref::kPoolingHw, func, inputData, outputData, inputShape[0],
+                 inputShape[chanIdx], inputShape[rowIdx], inputShape[colIdx],
+                 inputShape.getPadding(3), outputShape[rowIdx],
+                 outputShape[colIdx], outputShape.getPadding(3), poolRowSize,
+                 poolColSize, poolRowStride, poolColStride);
 }
 
 }  // namespace smaug
