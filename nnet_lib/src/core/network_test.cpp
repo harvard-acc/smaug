e021c030409e24aae8073e53f049ec0c9871f31b
Yuan Yao <yuanyao@seas.harvard.edu>
2019-06-04 12:27:06 -0400

    tests: Add Minerva network.
    This adds the 4-layer Minerva network as a unit test.
    
    TESTED=unit
    
    Change-Id: I16cfb671ef0b17c2c345951152b9c2f17f0af6a4

--- nnet_lib/src/core/network_test.cpp
+++ nnet_lib/src/core/network_test.cpp
@@ -9,7 +9,27 @@ using namespace smaug;
 
 TEST_CASE_METHOD(SmaugTest, "Network tests", "[network]") {
     std::string modelPath = "../../models/";
-    SECTION("ELU network. 11 layers of convolutions") {
+    SECTION("Minerva network. 4 layers of FCs.") {
+        // Minerva network with the SMV backend.
+        Network* network =
+                buildNetwork(modelPath + "minerva/minerva_smv_topo.pbtxt",
+                             modelPath + "minerva/minerva_smv_params.pb");
+        Tensor* output = runNetwork(network, workspace());
+
+        // The same network with the reference backend. This is used for
+        // producing expected outputs.
+        Network* refNetwork =
+                buildNetwork(modelPath + "minerva/minerva_ref_topo.pbtxt",
+                             modelPath + "minerva/minerva_ref_params.pb");
+        Tensor* refOutput = runNetwork(refNetwork, workspace());
+
+        // SMV outputs need to be converted into float32 before validations.
+        verifyOutputs<float>(
+                convertFp16ToFp32Tensor(output, workspace()), refOutput);
+    }
+
+    SECTION("ELU network. 11 layers of convolutions and 5 layers of "
+            "poolings.") {
         // ELU network with the SMV backend.
         Network* network =
                 buildNetwork(modelPath + "cifar100-elu/elu_smv_topo.pbtxt",
