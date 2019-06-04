652c03274b592546bb48056b44618814254db942
Yuan Yao <yuanyao@seas.harvard.edu>
2019-06-04 10:08:36 -0400

    smv: Fix the transfer size alignment in FP16 data DMA store.
    It should be aligned with CACHELINE_SIZE, since the smallest DMA
    transfer size is one cacheline.
    
    TESTED=unit
    
    Change-Id: I4c6cebe5f73472e7fe0915c448b455be3cedc0cc

--- nnet_lib/src/operators/smv/kernels/load_store_fp16_data.h
+++ nnet_lib/src/operators/smv/kernels/load_store_fp16_data.h
@@ -88,7 +88,8 @@ static inline void dma_store_fp16(float* local_data,
     VEC_ARRAY_1D(v8fp_t, _local_data_sp, local_data);
     const int page_size = (1 << LOG_PAGE_SIZE);
     const int max_transfer_size = page_size;
-    const int total_bytes = next_multiple(num_elems * sizeof(float16), 16);
+    const int total_bytes =
+            next_multiple(num_elems * sizeof(float16), CACHELINE_SIZE);
     int num_xfers = FRAC_CEIL(total_bytes, max_transfer_size);
     int num_bytes_remaining = total_bytes;
     dma_fp32_to_fp16:
@@ -108,7 +109,6 @@ static inline void dma_store_fp16(float* local_data,
             _local_data_hp[page_offset_vec * 2 + v] = fp16_data;
         }
 
-        transfer_size = next_multiple(transfer_size, 32);
         dmaStore(remote_data + remote_offset + curr_offset,
                  local_data + local_offset + curr_offset,
                  transfer_size);
