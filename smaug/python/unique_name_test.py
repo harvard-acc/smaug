fc81a33703bb968cbb937e11a542c4939987d6dc
Yuan Yao <5877112+yaoyuannnn@users.noreply.github.com>
2020-07-01 21:16:38 -0400

    python: Fix a bug in creating unique names. (#15)
    When creating a unique name for the operator, the naming format is
    "%(name)s_%(idx)d". If the name is already in use, we previously just
    increased the idx. For example, if "add" is in use, "add_1" will be
    used. However, this could mistakenly return a name already in use
    as shown in the following example:
    
      res = add(x, y) # This will be named "add".
      res = add(x, y, name="add_1") # This will be named "add_1".
      res = add(x, y) # BUG: this will also be named "add_1".
    
    To solve this, we keep increasing the idx until the new name is not
    in use. In the above example, the third add will now be named "add_2".
    
    This also makes create_unique_name() public, so the user can get a
    unique name before the operator is created. Example:
    
      res = add(x, y) # This will be named "add".
      # Get a unique name for the operator to be added. Note that
      # `mark_as_used` is set to False in order to avoid re-creating a
      # name for the operator. This returns "add_1".
      unique_name =
          global_vars.get_graph().create_unique_name(
              "add", mark_as_used=False)
      res = add(res, res, name=unique_name) # This will be named "add_1".
    
    TESTED=unit

---
+++ smaug/python/unique_name_test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+""" This tests the unique name creation in the Graph class."""
+
+import unittest
+import numpy as np
+
+from smaug.core import types_pb2
+from smaug.python.tensor import Tensor
+from smaug.python.graph import Graph
+from smaug.python.ops import math_ops
+
+graph_name = "test_graph"
+backend = "Reference"
+x = Tensor(
+    data_layout=types_pb2.N, tensor_data=np.random.rand(4).astype(np.float32))
+y = Tensor(
+    data_layout=types_pb2.N, tensor_data=np.random.rand(4).astype(np.float32))
+
+def get_node_names(graph):
+  return set([node.name for node in graph.get_nodes()])
+
+class TestUniqueName(unittest.TestCase):
+  def test_default_names(self):
+    with Graph(graph_name, backend) as test_graph:
+      res= math_ops.add(x, y)
+      res = math_ops.mul(x, res)
+    self.assertEqual(get_node_names(test_graph), {"add", "mul"})
+
+  def test_auto_unique_names(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y)
+      res = math_ops.add(res, res)
+      res = math_ops.add(res, res)
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1", "add_2"})
+
+  def test_user_supplied_names0(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      res = math_ops.mul(res, res, name="mul")
+    self.assertEqual(get_node_names(test_graph), {"add", "mul"})
+
+  def test_user_supplied_names1(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      res = math_ops.add(res, res, name="add_1")
+      res = math_ops.add(res, res, name="add")
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1", "add_2"})
+
+  def test_user_supplied_names1(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      res = math_ops.add(res, res, name="add_3")
+      res = math_ops.add(res, res, name="add")
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1", "add_3"})
+
+  def test_user_supplied_names2(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      res = math_ops.add(res, res, name="add_1")
+      res = math_ops.add(res, res, name="add_1")
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1", "add_1_1"})
+
+  def test_user_supplied_names3(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      unique_name = test_graph.create_unique_name("add", mark_as_used=False)
+      res = math_ops.add(res, res, name=unique_name)
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1"})
+
+  def test_user_supplied_names4(self):
+    with Graph(graph_name, backend) as test_graph:
+      res = math_ops.add(x, y, name="add")
+      unique_name = test_graph.create_unique_name("add", mark_as_used=False)
+      res = math_ops.add(res, res, name=unique_name)
+      res = math_ops.add(res, res, name="add")
+    self.assertEqual(get_node_names(test_graph), {"add", "add_1", "add_2"})
+
+if __name__ == "__main__":
+  unittest.main()
