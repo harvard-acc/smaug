b8b3d09f6ba49997999c6244e60cb171529c4e50
Yuan Yao <yuanyao@seas.harvard.edu>
2020-08-01 19:51:59 -0400

    python: Simplify Python module naming for easier usage.
    This makes the Python API easier to use. For example, previously the Python code
    looks like:
    
    ```python
    import numpy as np
    from smaug.python.ops import array_ops, data_op, math_ops, nn_ops
    from smaug.python.tensor import Tensor
    from smaug.python.graph import Graph
    from smaug.core import types_pb2
    
    with Graph():
      x = Tensor(layout=types_pb2.NC, tensor_data=np.array())
      x = data_op.input_data(x)
      y = math_ops.add(x, x)
      z = array_ops.flatten(y)
      u = nn_ops.matmul(..., activation=types_pb2.ReLU)
    ```
    Now it becomes:
    
    ```python
    import numpy as np
    import smaug as sg
    
    with sg.Graph():
      x = sg.Tensor(layout=sg.NC, tensor_data=np.array())
      x = sg.input_data(x)
      y = sg.math.add(x, x)
      z = sg.flatten(y)
      u = sg.nn.matmul(..., activation=sg.ReLU)
    ```
    
    This fixes issue #31.

---
+++ smaug/__init__.py
@@ -0,0 +1,10 @@
+from smaug.core.types_pb2 import *
+
+from smaug.python.ops import math_ops as math
+from smaug.python.ops import nn
+from smaug.python.ops.array_ops import *
+from smaug.python.ops.control_flow_ops import cond
+from smaug.python.ops.data_op import input_data
+
+from smaug.python.graph import Graph
+from smaug.python.tensor import Tensor
