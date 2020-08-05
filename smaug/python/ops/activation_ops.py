c87ecd00975c518e83a012e310b0548acfd351e3
Yuan Yao <yuanyao@seas.harvard.edu>
2020-08-05 17:21:19 -0400

    python: A little fix.

--- smaug/python/ops/activation_ops.py
+++ smaug/python/ops/activation_ops.py
@@ -3,17 +3,6 @@ from smaug.core import node_pb2
 from smaug.python import global_vars
 from smaug.python.ops import common
 
-_activation_types = {
-    "relu": types_pb2.ReLU,
-    "lrelu": types_pb2.LReLU,
-    "elu": types_pb2.ELU,
-    "selu": types_pb2.SELU,
-    "tanh": types_pb2.Tanh,
-    "hard_tanh": types_pb2.HardTanh,
-    "sigmoid": types_pb2.Sigmoid,
-    "softmax": types_pb2.Softmax
-}
-
 def _set_activation_params(activation, params, proto):
   """Set the parameters of the activation function.
 
@@ -52,23 +41,7 @@ def get_activation_op(activation):
   Args:
     activation: A string representing the activation function.
   """
-  op_type = _activation_types[activation]
-  if op_type == types_pb2.ReLU:
-    return relu
-  elif op_type == types_pb2.LReLU:
-    return lrelu
-  elif op_type == types_pb2.ELU:
-    return elu
-  elif op_type == types_pb2.SELU:
-    return selu
-  elif op_type == types_pb2.Tanh:
-    return tanh
-  elif op_type == types_pb2.HardTanh:
-    return hard_tanh
-  elif op_type == types_pb2.Sigmoid:
-    return sigmoid
-  elif op_type == types_pb2.Softmax:
-    return softmax
+  return _activation_type_op_tuples[activation][1]
 
 def to_proto(activation, params):
   """Return the activation proto.
@@ -82,7 +55,7 @@ def to_proto(activation, params):
     An `ActivationParams`, the proto.
   """
   proto = node_pb2.ActivationParams()
-  act_type = _activation_types[activation]
+  act_type = _activation_type_op_tuples[activation][0]
   proto.activation = act_type
   _set_activation_params(act_type, params, proto)
   return proto
@@ -146,3 +119,14 @@ def softmax(input_tensor, name=None):
       name=name, op=types_pb2.Softmax, input_tensors=[input_tensor],
       output_tensors_dims=[input_tensor.shape.dims],
       output_tensor_layout=input_tensor.shape.layout)[0]
+
+_activation_type_op_tuples = {
+    "relu": (types_pb2.ReLU, relu),
+    "lrelu": (types_pb2.LReLU, lrelu),
+    "elu": (types_pb2.ELU, elu),
+    "selu": (types_pb2.SELU, selu),
+    "tanh": (types_pb2.Tanh, tanh),
+    "hard_tanh": (types_pb2.HardTanh, hard_tanh),
+    "sigmoid": (types_pb2.Sigmoid, sigmoid),
+    "softmax": (types_pb2.Softmax, softmax)
+}
