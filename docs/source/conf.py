ed10c4233e5da9805c9a0911fc9884746ca06d26
Yuan Yao <5877112+yaoyuannnn@users.noreply.github.com>
2020-08-11 23:20:44 -0400

    docs: Fix Sphinx docs deployment. (#59)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,11 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 import textwrap
-
-sys.path.insert(0, os.path.abspath('../../'))
-
 
 # -- Project information -----------------------------------------------------
 
